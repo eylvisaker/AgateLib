496914ff4bbfbf252a61a11a058758f3d15f7a4d
Erik Ylvisaker <eylvisaker@gmail.com>
2006-09-02 17:23:06 +0000

    Added examples

---
+++ Examples/CreateWindowTest/Program.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using ERY.AgateLib;
+
+namespace ERY.CreateWindowTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // If you are debugging this in visual studio 2005, be sure to turn off
+            // LoaderLock exceptions from occuring.
+            // It's under Debug->Exceptions->Managed Debug Assistance->LoaderLock.
+
+            // Creates an AgateSetup object to create and setup the Agate library.
+            using (AgateSetup setup = new AgateSetup())
+            {
+                // initialize the display, asking the user what display driver to use.
+                setup.Initialize(true, false, false);
+                
+                // normally, the display should initialize fine.
+                // However, here we are asking the user what display mode they want to pick,
+                // and they may push the cancel button.  If they do, then exit the program.
+                if (setup.Cancel)
+                    return;
+
+                // This creates the window that we will be drawing in.
+                // 640x480 are the dimensions of the screen area that we will write to
+                DisplayWindow wind = new DisplayWindow("Initialize Example", 640, 480, false, true);
+
+
+                // create a random number generation object 
+                // so that we can make pretty colors.
+                Random rand = new Random();
+
+                while (wind.Closed == false)
+                {
+                    // Display.BeginFrame must be called before any rendering takes place.
+                    Display.BeginFrame();
+
+                    // Clear back buffer with red
+                    Display.Clear(Color.Red);
+
+                    // draw random lines and boxes
+
+                    // line drawn starts at (0, 0), the upper left corner.
+                    Display.DrawLine(
+                        0, 0,
+                        rand.Next(10, 700), rand.Next(10, 700),
+                        Color.Black);
+
+                    Display.DrawRect(
+                        new Rectangle(
+                        rand.Next(0, 540), rand.Next(0, 380),
+                        rand.Next(50, 200), rand.Next(50, 200)),
+                        Color.Black);
+
+                    Display.FillRect(
+                        new Rectangle(
+                        rand.Next(0, 540), rand.Next(0, 380),
+                        rand.Next(50, 200), rand.Next(50, 200)),
+                        Color.Black);
+
+                    // Display.EndFrame must be called after rendering is done
+                    // in order to actually update the display.
+                    Display.EndFrame();
+
+                    // Core.KeepAlive is where we play nice window the OS, 
+                    // allowing events to be processed and such.
+                    // This is also required to process events that happen in our OWN 
+                    // code (ie. user input), so be sure to call this once a frame.
+                    Core.KeepAlive();
+
+                    // This gives a 100 millisecond delay between each frame.
+                    // Using the Sleep() call causes this application to
+                    // relinquish CPU time.
+                    System.Threading.Thread.Sleep(100);
+
+                    // toggle full screen if the user pressed F5
+                    if (Keyboard.Keys[KeyCode.F5])
+                    {
+                        Display.CurrentWindow.ToggleFullScreen();
+
+                        // make that we used this keypress
+                        Keyboard.Keys[KeyCode.F5] = false;
+                    }
+                    
+                    // and exit the application if the user pressed escape.
+                    if (Keyboard.Keys[KeyCode.Escape])
+                    {
+                        return;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Examples/MultipleWindowTest/Program.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using ERY.AgateLib;
+
+namespace MultipleWindowTest
+{
+    static class Program
+    {
+        static Surface surf;
+        static Random rand = new Random();
+
+
+        [STAThread]
+        static void Main()
+        {
+
+            using (AgateSetup setup = new AgateSetup())
+            {
+                setup.Initialize(true, false, false);
+                if (setup.Cancel) return;
+
+                Form1 myForm = new Form1();
+                myForm.Show();
+
+                // create three display windows
+                DisplayWindow wnd_1 = new DisplayWindow(myForm.pictureBox1);
+                DisplayWindow wnd_2 = new DisplayWindow(myForm.pictureBox2);
+                DisplayWindow wnd_3 = new DisplayWindow(myForm.pictureBox3);
+
+                // create a surface for drawing
+                surf = new Surface(300, 300);
+
+                // this is the code that will be called when the button is pressed
+                myForm.button1.Click += new EventHandler(button1_Click);
+
+                while (myForm.Visible)
+                {
+                    // Render targets must be set before the call to BeginFrame,
+                    // and may not be changed between BeginFrame and EndFrame.
+                    Display.RenderTarget = wnd_1;
+
+                    Display.BeginFrame();
+                    Display.Clear(Color.Red);
+                    Display.FillRect(new Rectangle(20, 20, 40, 30), Color.Blue);
+                    Display.EndFrame();
+
+                    // now do the second window.
+                    Display.RenderTarget = wnd_2;
+
+                    Display.BeginFrame();
+                    Display.Clear(Color.Green);
+                    Display.FillRect(new Rectangle(20, 20, 40, 30), Color.Yellow); 
+                    Display.EndFrame();
+
+                    // draw the third window from the surface
+                    Display.RenderTarget = wnd_3;
+
+                    Display.BeginFrame();
+                    Display.Clear(Color.Black);
+                    surf.Draw(0, 0);
+                    Display.EndFrame();
+
+                    Core.KeepAlive();
+                    System.Threading.Thread.Sleep(250);
+
+                }
+
+            }
+
+
+        }
+
+        static void button1_Click(object sender, EventArgs e)
+        {
+            Display.RenderTarget = surf;
+
+            Display.BeginFrame();
+
+            Rectangle rect = new Rectangle(
+                rand.Next(-10, 190),
+                rand.Next(-10, 140),
+                rand.Next(20, 100),
+                rand.Next(20, 100));
+            Color clr = Color.FromArgb(rand.Next(200, 256), rand.Next(0, 256),
+                    rand.Next(0, 256), rand.Next(0, 256));
+
+            Display.FillRect(rect, clr);
+
+            Display.EndFrame();
+
+            System.Diagnostics.Debug.Print("Wrote rectangle to {0} with color {1}.", rect, clr);
+            System.Diagnostics.Debug.Flush();
+        }
+
+    }
+}
