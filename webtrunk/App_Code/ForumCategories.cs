36d7506f67638ce9d466faf26df584fceb368208
mreiland <mreiland@e953d628-571c-0410-b8f7-890e41260987>
2006-11-13 23:09:22 +0000

    CreateCategory now assigns a new position automagically.

--- webtrunk/App_Code/ForumCategories.cs
+++ webtrunk/App_Code/ForumCategories.cs
@@ -66,21 +66,42 @@ public class CategoryDB
 
     // TODO: check contents of cat_details
     // TODO: find new id and return cat_details
-    public void InsertCategory( CategoryDetails cat_details )
+    // TODO: check that cat_name is < 50 characters
+
+    /*
+     * Create connection, query for the max position, update db with new category of position + 1
+     */
+    public void InsertCategory( String cat_name )
     {
         DbConnection conn = new DbConnection(conn_string);
 
+        // Position is a Jet Reserved Word
+        DbCommand max_position_cmd = new DbCommand("SELECT MAX([Position]) FROM ForumCategories", conn);
+
         String query_string = "INSERT INTO ForumCategories "+
             " (Name, [Position], CreationDate)" +
-            " Values('" + cat_details.Name + "'," + cat_details.Position + ",'" + DateTime.Now.ToString() + "')";
+            " Values(?,?,?)";
 
-        DbCommand cmd = new DbCommand(query_string, conn);
-
+        DbDataReader reader = null;
 
         try
         {
-            conn.Open();
 
+            conn.Open();
+            reader = max_position_cmd.ExecuteReader();
+
+            // If there are no Categories in the table, this will be incremented to 0.
+            int max_position = -1;
+
+            if( reader.Read() && !reader.IsDBNull(0) )
+            {
+                max_position = reader.GetInt32(0);
+            }
+
+            DbCommand cmd = new DbCommand(query_string, conn);
+                cmd.Parameters.Add("@Name", DbType.WChar, 50).Value = cat_name;
+                cmd.Parameters.Add("@Position", DbType.Integer).Value = max_position + 1;
+                cmd.Parameters.Add("@CreationDate", DbType.Date).Value = DateTime.Now.ToString();
             cmd.ExecuteNonQuery();
         }
         catch (DbException e)
@@ -95,7 +116,7 @@ public class CategoryDB
     }
 
 
-    public void UpdateCategory(CategoryDetails cat_details)
+    public void DeleteCategory(CategoryDetails cat_details)
     {
 
     }
