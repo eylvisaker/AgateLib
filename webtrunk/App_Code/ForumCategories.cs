af349fd7b0ef796deea38f7eade909935cc5dda5
mreiland <mreiland@e953d628-571c-0410-b8f7-890e41260987>
2006-11-25 21:11:22 +0000

    * Initial creation of TreatAsListHelper. * Created/tested TreatAsListHelper.MoveToFirst * Created/tested TreatAsListHelper.MoveToLast

--- webtrunk/App_Code/ForumCategories.cs
+++ webtrunk/App_Code/ForumCategories.cs
@@ -17,46 +17,106 @@ using DbTransaction = System.Data.OleDb.OleDbTransaction;
 using DbException = System.Data.OleDb.OleDbException;
 using DbDataAdapter = System.Data.OleDb.OleDbDataAdapter;
 
-
 /*
- * This is a POD.  All actual constraints will be checked in CategoryDB 
+ * The following class if *VERY* fragile.  There are many assumptions/requirements with regards to the data
+ *  that it handles.  If any of these are broken, or changed, it *will* result in unexpected behavior.
  * 
+ * DATASET TABLE LAYOUT
+ *  This requires a primary key column of "id" & a position identifier column of "Position", both of which are type int.
+ *   These can be generalized to get around this requirement, but for the sake of brevity, it has not been done.
+ * 
+ *  POSITION COLUMN CONTAINS STRICTLY CONTIGUOUS INTEGERS
+ *   This is very very important.  Every movement operation we do works under the assumption that everything the 
+ *   position column data is contiguous.
+ *   
  */
-
-public class Category
+public class TreatAsListHelper
 {
+    private DataSet data_set;
+    private String table_name;
 
 
-}
-
-public class CategoryList
-{
-    public String Name;
-    public int Position;
-    public DateTime CreationDate;
-
-    internal int pID;
-
-
-    private DataSet ds_category_list;
-
-    public CategoryList()
+    public TreatAsListHelper( DataSet ds, String tbl_name )
     {
-        ds_category_list = CategoryDB.GetAllCategories();
-        // signifies that it hasn't been set yet
-        pID = -1;
+        data_set = ds;
+        table_name = tbl_name;
     }
 
-    public void Save()
+    public DataSet GetUnderlyingDataSet()
     {
-
+        return data_set;
     }
 
-    
 
-    public int ID
+
+    public void MoveToFirst(int row_id)
     {
-        get { return pID; }
+        DataRow[] rows_with_id = data_set.Tables[table_name].Select("id = '" + row_id.ToString() + "'" );
+
+        if (rows_with_id.Length == 1)
+        {
+            DataRow row_to_be_moved = rows_with_id[0];
+
+            int current_position = (int)row_to_be_moved["Position"];
+
+            if ( current_position > 0)
+            {
+                String select_string = "Position < '" + current_position.ToString() + "'";
+                DataRow[] rows = data_set.Tables[table_name].Select( select_string );
+
+                foreach (DataRow row in rows)
+                    row["Position"] = (int)row["Position"] + 1;
+
+                row_to_be_moved["Position"] = 0;
+            }
+        }
+        else if (rows_with_id.Length > 1)
+        {
+            throw new System.ApplicationException("DataSet is showing duplicated primary keys");
+        }
+    }
+
+
+
+    public void MoveToLast(int row_id)
+    {
+        DataRow[] rows_with_id = data_set.Tables[table_name].Select("id = '" + row_id.ToString() + "'");
+
+        if (rows_with_id.Length == 1)
+        {
+            DataRow row_to_be_moved = rows_with_id[0];
+
+            int current_position = (int)row_to_be_moved["Position"];
+
+            // super ugly hack to get the number of rows in the table
+            if (current_position < data_set.Tables[table_name].Rows.Count - 1  )
+            {
+                String select_string = "Position > '" + current_position.ToString() + "'";
+                DataRow[] rows = data_set.Tables[table_name].Select(select_string);
+
+                foreach (DataRow row in rows)
+                    row["Position"] = (int)row["Position"] - 1;
+
+                row_to_be_moved["Position"] = data_set.Tables[table_name].Rows.Count - 1;
+            }
+        }
+        else if (rows_with_id.Length > 1)
+        {
+            throw new System.ApplicationException("DataSet is showing duplicated primary keys");
+        }
+    }
+
+
+    public DataSet MoveUp(int row_id)
+    {
+        return data_set;
+    }
+
+
+
+    public DataSet MoveDown(int row_id)
+    {
+        return data_set;
     }
 }
 
@@ -80,14 +140,6 @@ public static class CategoryDB
         connection = new DbConnection(conn_string);
 
     }
-
-    //public static CategoryDetails GetCategory( String cat_name )
-    //{
-        //CategoryDetails tmpDetails = new CategoryDetails();
-
-
-//        return tmpDetails;
-  //  }
 
 
     //TODO: Implement roles based authority
@@ -168,6 +220,8 @@ public static class CategoryDB
         }
     }
 
+
+
     /*
      * Algorithm:
      *  Read in dataset, remove category, decrement all categories with positions > category just removed,
@@ -188,12 +242,69 @@ public static class CategoryDB
         {
             DataRow dr = dr_collection[0];
 
-            int curr_position;
+            int curr_position = -1;
 
             if ((String)dr["Name"] == cat_name)
             {
                 curr_position = (int)dr["Position"];
             }
+
+
+            // Create an explicit UPDATE command
+            DbDataAdapter da = new DbDataAdapter();
+            da.UpdateCommand = connection.CreateCommand();
+
+
+            da.UpdateCommand.CommandText =
+                "UPDATE ForumCategories " +
+                "SET [Position] = ?" + 
+                "WHERE id = ?";
+
+            // Bind parameters to appropriate columns for UPDATE command
+            da.UpdateCommand.Parameters.Add("@Position", DbType.Integer, 0, "Position");
+            da.UpdateCommand.Parameters.Add("@id", DbType.Integer, 0, "id");
+
+            
+            // Create an explicit DELETE command
+            da.DeleteCommand = connection.CreateCommand();
+            da.DeleteCommand.CommandText =
+                "DELETE FROM ForumCategories " +
+                "WHERE id = ?";
+
+
+            // Bind parameters to appropriate columns for DELETE command
+            da.DeleteCommand.Parameters.Add("@id", DbType.Integer, 0, "id");
+
+            dr.Delete();
+
+            da.Update(ds.Tables[0].GetChanges());
+            ds.AcceptChanges();
+
+            ds = GetAllCategories();
+
+            
+            // not sure if this works
+            foreach (DataRow row in ds.Tables[0].Select("[Position] > '" + curr_position.ToString() + "'" ) )
+            {
+                if ( curr_position != -1 )
+                {
+                    int tmp = (int)row["Position"];
+                    row["Position"] = (tmp - 1).ToString();
+
+                    
+                }
+            }
+
+            DataTable changes = ds.Tables[0].GetChanges();
+
+            if (changes != null)
+            {
+
+                da.Update(ds.Tables[0].GetChanges());
+                ds.AcceptChanges();
+            }
+            
+
         }
         // if category was not 
     }
