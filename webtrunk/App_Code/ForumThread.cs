69dc14569771d4107f1b4179d7d353233670076e
mreiland <mreiland@e953d628-571c-0410-b8f7-890e41260987>
2006-12-03 00:30:33 +0000

    Created file: App_Data/ForumThread.cs changed DB table name ForumThread to ForumThreads

---
+++ webtrunk/App_Code/ForumThread.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+using DbConnection = System.Data.OleDb.OleDbConnection;
+using DbCommand = System.Data.OleDb.OleDbCommand;
+using DbType = System.Data.OleDb.OleDbType;
+using DbDataReader = System.Data.OleDb.OleDbDataReader;
+using DbParameter = System.Data.OleDb.OleDbParameter;
+using DbTransaction = System.Data.OleDb.OleDbTransaction;
+using DbException = System.Data.OleDb.OleDbException;
+using DbDataAdapter = System.Data.OleDb.OleDbDataAdapter;
+
+/// <summary>
+///  Static Data Access Layer for the ForumThreads table
+/// </summary>
+public class ForumThreadDAL
+{
+    private static string table_name = "ForumThreads";
+
+	public ForumThreadDAL()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
