bc45aa91dd145c5fc200bcf0b68cf2bd13c4c552
mreiland <mreiland@e953d628-571c-0410-b8f7-890e41260987>
2006-12-03 09:45:32 +0000

    renamed App_Code/ForumThread.cs to App_Code/ForumThreads.cs Created App_Code/ForumPosts.cs

---
+++ webtrunk/App_Code/ForumPosts.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+/// <summary>
+/// Summary description for ForumPosts
+/// </summary>
+public static class PostDAL
+{
+    private static String table_name = "ForumPosts";
+}
