b586fe2d06cc293a8d95964ed633dcb91b773e2c
Erik Ylvisaker <eylvisaker@gmail.com>
2008-03-01 23:55:58 +0000

    Organized tests by subsystem.

---
+++ Tests/Core/RefCounterTester/Program.cs
@@ -0,0 +1,47 @@
+// The contents of this file are public domain.
+// You may use them as you wish.
+//
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ERY.AgateLib;
+using ERY.AgateLib.Utility;
+
+namespace RefCounterTester
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Ref<TestClass> myref = new Ref<TestClass>(new TestClass());
+            Ref<TestClass> newref = new Ref<TestClass>(myref);
+
+
+            newref.Dispose();
+            myref.Dispose();
+
+            Console.ReadKey(false);
+        }
+
+        class TestClass : IDisposable
+        {
+            public TestClass()
+            {
+                System.Console.WriteLine("TestClass Created.");
+            }
+            ~TestClass()
+            {
+                System.Console.WriteLine("TestClass Destroyed.");
+            }
+
+            public void Dispose()
+            {
+                System.Console.WriteLine("TestClass Disposed.");
+            }
+        }
+    }
+}
