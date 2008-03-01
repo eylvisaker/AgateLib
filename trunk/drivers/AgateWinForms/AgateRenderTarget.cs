65c9a267b509d393a1abd5928438ef2890d949f3
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-03-01 09:20:21 +0000

    Set class style to include OWNDC.

--- trunk/drivers/AgateWinForms/AgateRenderTarget.cs
+++ trunk/drivers/AgateWinForms/AgateRenderTarget.cs
@@ -11,10 +11,6 @@ namespace ERY.AgateLib.WinForms
     /// <summary>
     /// A generic render target control for AgateLib to render to.
     /// </summary>
-    /// <remarks>
-    /// [Experimental - This class will be moved to into a different assembly
-    /// in the future.]
-    /// </remarks>
     public partial class AgateRenderTarget : Panel
     {
         /// <summary>
@@ -26,10 +22,40 @@ namespace ERY.AgateLib.WinForms
             SetStyle(ControlStyles.Opaque, true);                          // No Need To Draw Form Background
             SetStyle(ControlStyles.ResizeRedraw, true);                    // Redraw On Resize
             SetStyle(ControlStyles.UserPaint, true);                       // We'll Handle Painting Ourselves
-
+            
             InitializeComponent();
         }
 
+        enum ClassStyles : uint
+        {
+            CS_VREDRAW = 0x0001,
+            CS_HREDRAW = 0x0002,
+            CS_DBLCLKS = 0x0008,
+            CS_OWNDC = 0x0020,
+            CS_CLASSDC = 0x0040,
+            CS_PARENTDC = 0x0080,
+            CS_NOCLOSE = 0x0200,
+            CS_SAVEBITS = 0x0800,
+            CS_BYTEALIGNCLIENT = 0x1000,
+            CS_BYTEALIGNWINDOW = 0x2000,
+            CS_GLOBALCLASS = 0x4000,
+            CS_IME = 0x00010000,
+            CS_DROPSHADOW = 0x00020000
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                // supposedly this should improve compatibility on Windows Vista
+                // when the compositing manager is used and Agate is running through
+                // OpenGL.
+                CreateParams retval = base.CreateParams;
+                retval.ClassStyle |= (int)ClassStyles.CS_OWNDC;
+
+                return retval;
+            }
+        }
         /// <summary>
         /// Overrides the base class OnPaint method, to clear the
         /// client area in the Visual Studio designer.
