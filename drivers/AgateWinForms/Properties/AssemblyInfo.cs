e0c06dff31a9b7375b987a275928768e913ce336
Erik Ylvisaker <eylvisaker@gmail.com>
2008-03-30 21:41:19 +0000

    Correct names

--- drivers/AgateWinForms/Properties/AssemblyInfo.cs
+++ drivers/AgateWinForms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AgateForms")]
+[assembly: AssemblyTitle("AgateWinForms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AgateForms")]
+[assembly: AssemblyProduct("AgateWinForms")]
 [assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
