e1e90365e61289f2ea3cc14491b0fc859c889780
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-07-06 19:36:21 +0000

    Added ResourceTester.

---
+++ trunk/Tests/Core/ResourceTester/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ERY.AgateLib;
+using ERY.AgateLib.Geometry;
+using ERY.AgateLib.Resources;
+
+namespace ResourceTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AgateSetup setup = new AgateSetup("Resource Tester", args))
+            {
+                setup.InitializeAll();
+                if (setup.Cancel)
+                    return;
+
+                AgateResourceManager resources = new AgateResourceManager();
+                resources.Load("test.xml");
+
+            }
+        }
+    }
+}
