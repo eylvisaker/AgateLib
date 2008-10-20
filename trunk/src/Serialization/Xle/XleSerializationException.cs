40d1f615bf884c1927e4e5bf51036a1b45a3ddee
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-10-20 18:22:09 +0000

    Added documentation.

--- trunk/src/Serialization/Xle/XleSerializationException.cs
+++ trunk/src/Serialization/Xle/XleSerializationException.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace ERY.AgateLib.Serialization.Xle
 {
     /// <summary>
-    /// 
+    /// Exception thrown when there is a problem in the XleSerializer.
     /// </summary>
     [global::System.Serializable]
     public class XleSerializationException : Exception
@@ -17,9 +17,14 @@ namespace ERY.AgateLib.Serialization.Xle
         //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
         //
 
-        public XleSerializationException() { }
-        public XleSerializationException(string message) : base(message) { }
-        public XleSerializationException(string message, Exception inner) : base(message, inner) { }
+        internal XleSerializationException() { }
+        internal XleSerializationException(string message) : base(message) { }
+        internal XleSerializationException(string message, Exception inner) : base(message, inner) { }
+        /// <summary>
+        /// Constructs an XleSerializationException object when deserializing it.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         protected XleSerializationException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context)
