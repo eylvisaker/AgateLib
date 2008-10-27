85ed1023cd2614ec6d708674c995ebcf53ceff11
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-10-27 17:07:28 +0000

    Added AgateException and AgateResourceException.

---
+++ trunk/src/Core/AgateException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERY.AgateLib
+{
+    [global::System.Serializable]
+    public class AgateException : ApplicationException
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public AgateException() { }
+        public AgateException(string message) : base(message) { }
+        public AgateException(string message, Exception inner) : base(message, inner) { }
+        protected AgateException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+
+}
