2053950b792509c6488c3bf5ca4d49a3a0ca4f22
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-12-06 20:16:07 +0000

    Update nant.sh file to make backslashes work in resx files.

--- branches/build/Build/nant.sh
+++ branches/build/Build/nant.sh
@@ -4,6 +4,11 @@ mono Prebuild/Prebuild.exe /target nant /file AgateLib.xml
 mono Prebuild/Prebuild.exe /target nant /file AgateTests.xml
 mono Prebuild/Prebuild.exe /target nant /file AgateTools.xml
 
+# This environment variable allows mono's resgen
+# to use backslashes as path separators, making it
+# compatible with resx files generated with Visual Studio.
+export MONO_IOMAP=all
+
 nant -buildfile:AgateLib.build
 nant -buildfile:AgateTests.build
 nant -buildfile:AgateTools.build
