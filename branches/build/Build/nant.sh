9c2024d5f25a16794e4cbc539377ec003549c3a5
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-12-06 19:26:20 +0000

    Updated build system to work on Linux.

--- branches/build/Build/nant.sh
+++ branches/build/Build/nant.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 mono Prebuild/Prebuild.exe /target nant /file AgateLib.xml 
-mono Prebuild/Prebuild.exe /target nant /file AgateLib-Windows.xml 
 mono Prebuild/Prebuild.exe /target nant /file AgateTests.xml
 mono Prebuild/Prebuild.exe /target nant /file AgateTools.xml
 
 nant -buildfile:AgateLib.build
-#nant -buildfile:AgateTests.build
-#nant -buildfile:AgateTools.build
+nant -buildfile:AgateTests.build
+nant -buildfile:AgateTools.build
 
