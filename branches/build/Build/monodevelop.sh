f40466562165851eb87c2d3d15e12f54c3ca1877
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-12-07 07:19:51 +0000

    Added monodevelop.sh file.

---
+++ branches/build/Build/monodevelop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mono Prebuild/Prebuild.exe /target monodev /file AgateLib.xml 
+mono Prebuild/Prebuild.exe /target monodev /file AgateTests.xml
+mono Prebuild/Prebuild.exe /target monodev /file AgateTools.xml
+
