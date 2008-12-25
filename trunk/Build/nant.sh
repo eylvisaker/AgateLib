345cd82e32e235a72d7fed557e6d191351be824b
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2008-12-25 01:36:23 +0000

    Update nant.sh to build all projects. Update PackedSpriteCreator.proj.xml to copy AgateLib.dll locally when built.

--- trunk/Build/nant.sh
+++ trunk/Build/nant.sh
@@ -1,8 +1,9 @@
+<<<<<<< .mine
 #!/bin/bash
 
 mono Prebuild/Prebuild.exe /target nant /file AgateLib.xml 
-#mono Prebuild/Prebuild.exe /target nant /file AllTests.xml
-#mono Prebuild/Prebuild.exe /target nant /file AgateTools.xml
+mono Prebuild/Prebuild.exe /target nant /file AllTests.xml
+mono Prebuild/Prebuild.exe /target nant /file AgateTools.xml
 
 # This environment variable allows mono's resgen
 # to use backslashes as path separators, making it
@@ -10,7 +11,6 @@ mono Prebuild/Prebuild.exe /target nant /file AgateLib.xml
 export MONO_IOMAP=all
 
 nant -buildfile:AgateLib.build
-#nant -buildfile:AllTests.build
-#nant -buildfile:AgateTools.build
-
+nant -buildfile:AllTests.build
+nant -buildfile:AgateTools.build
 
