dcc5d7fe43aa53abcb17f9ab2ee061d1d9b916a2
Erik Ylvisaker <eylvisaker@gmail.com>
2008-12-29 16:53:47 +0000

    Update version numbers and copyright dates in AssemblyInfo.cs files.

--- Drivers/AgateMDX/Properties/AssemblyInfo.cs
+++ Drivers/AgateMDX/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AgateMDX")]
 [assembly: AssemblyDescription("Managed DirectX driver for AgateLib")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ERY")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AgateMDX")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
+[assembly: AssemblyCopyright("Copyright ©  2006-9")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.5.0")]
-[assembly: AssemblyFileVersion("0.2.5.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
