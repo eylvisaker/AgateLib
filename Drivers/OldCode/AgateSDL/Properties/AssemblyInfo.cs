344e7dc62aa4a191a2bf7f6851a9df73ea45b417
Erik Ylvisaker <eylvisaker@gmail.com>
2008-12-29 16:53:11 +0000

    Update version numbers and copyright dates in AssemblyInfo.cs files.

--- Drivers/OldCode/AgateSDL/Properties/AssemblyInfo.cs
+++ Drivers/OldCode/AgateSDL/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DisplayImpl_SDL")]
+[assembly: AssemblyTitle("AgateSDL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DisplayImpl_SDL")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
+[assembly: AssemblyProduct("AgateSDL")]
+[assembly: AssemblyCopyright("Copyright ©  2006-9")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
