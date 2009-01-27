91b889d2357b4eba969fe6364cea797f998650ee
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-01-27 19:08:42 +0000

    Added AgateLib.Gui to the build system. Deleted old GUI files. Added tester files to AgateLib.Gui.

---
+++ branches/registrar/Gui/Tester/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace AgateLib.Gui.Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.agateRenderTarget1 = new AgateLib.WinForms.AgateRenderTarget();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(669, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 448);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(669, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // agateRenderTarget1
+            // 
+            this.agateRenderTarget1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.agateRenderTarget1.Location = new System.Drawing.Point(0, 24);
+            this.agateRenderTarget1.Name = "agateRenderTarget1";
+            this.agateRenderTarget1.Size = new System.Drawing.Size(669, 446);
+            this.agateRenderTarget1.TabIndex = 0;
+            this.agateRenderTarget1.Paint += new System.Windows.Forms.PaintEventHandler(this.agateRenderTarget1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 470);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.agateRenderTarget1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AgateLib.WinForms.AgateRenderTarget agateRenderTarget1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ branches/registrar/Gui/Tester/RenderGui.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib.DisplayLib;
+using AgateLib.Geometry;
+
+namespace AgateLib.Gui.Tester
+{
+    class RenderGui
+    {
+        GuiRoot root = new GuiRoot();
+
+        public RenderGui()
+        {
+            CreateGui();
+        }
+
+        internal void Run()
+        {
+            root.DrawBehindGui += new EventHandler(root_DrawBehindGui);
+            root.Update += new EventHandler(root_Update);
+            root.Run();
+        }
+
+        Label fps;
+        void root_Update(object sender, EventArgs e)
+        {
+            fps.Text = Display.FramesPerSecond.ToString();
+        }
+
+        void root_DrawBehindGui(object sender, EventArgs e)
+        {
+            Display.Clear(Color.DarkBlue);
+        }
+
+        private void CreateGui()
+        {
+            //ThemeEngines.Graphite.Graphite.DebugOutlines = true;
+
+            Panel topPanel = new Panel();
+            Panel bottomPanel = new Panel();
+
+            Panel leftPanel = new Panel();
+            Panel rightPanel = new Panel();
+
+            for (int i = 0; i < 4; i++)
+            {
+                leftPanel.Children.Add(new TextBox());
+                rightPanel.Children.Add(new TextBox());
+            }
+
+            topPanel.Children.Add(leftPanel);
+            topPanel.Children.Add(rightPanel);
+            topPanel.Layout = new Layout.HorizontalBox();
+
+            fps = new Label();
+            bottomPanel.Children.Add(fps);
+
+            Window wind = new Window("Test Window");
+            wind.SuspendLayout();
+            wind.Children.Add(topPanel);
+            wind.Children.Add(bottomPanel);
+            wind.AllowDrag = true;
+            wind.Size = new Size(400, 300);
+            bottomPanel.LayoutExpand = LayoutExpand.ExpandToMax;
+
+            root.Children.Add(wind);
+            root.ResumeLayout();
+
+            System.Diagnostics.Debug.Assert(
+                leftPanel.PointToClient(leftPanel.PointToScreen(new Point(10, 8))) == new Point(10, 8));
+
+            Display.PackAllSurfaces();
+        }
+
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            switch (btn.LayoutExpand)
+            {
+                case LayoutExpand.Default:
+                    btn.LayoutExpand = LayoutExpand.ExpandToMax;
+                    break;
+
+                case LayoutExpand.ExpandToMax:
+                    btn.LayoutExpand = LayoutExpand.ShrinkToMin;
+                    break;
+
+                case LayoutExpand.ShrinkToMin:
+                    btn.LayoutExpand = LayoutExpand.Default;
+                    break;
+            } 
+        }
+    }
+}
