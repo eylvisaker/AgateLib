902e846b84d6e2100d64e4a0b5bdf2679d8ecfee
Erik Ylvisaker <eylvisaker@gmail.com>
2009-01-29 00:38:25 +0000

    Fix Otk_Reporter to workaround bug in gmcs.

--- Drivers/AgateOTK/Otk_Reporter.cs
+++ Drivers/AgateOTK/Otk_Reporter.cs
@@ -12,24 +12,26 @@ namespace AgateOTK
             yield return new AgateDriverInfo(
                  DisplayTypeID.OpenGL, typeof(GL_Display), "OpenGL through OpenTK 0.9.2", 1120);
 
-            bool reportOpenAL = true;
-
-            try
-            {
-                // test for the presence of working OpenAL.
-                new OpenTK.Audio.AudioContext().Dispose();
-
-            }
-            catch (Exception e)
-            {
-                reportOpenAL = false;
-            }
-
-            if (reportOpenAL)
+            if (ReportOpenAL())
             {
                 yield return new AgateDriverInfo(
                     AudioTypeID.OpenAL, typeof(AL_Audio), "OpenAL through OpenTK 0.9.2", 100);
             }
         }
+
+	bool ReportOpenAL()
+	{
+            try
+            {
+                // test for the presence of working OpenAL.
+                new OpenTK.Audio.AudioContext().Dispose();
+		return true;
+            }
+            catch (Exception e)
+            {
+	    	return false;
+            }
+
+	}
     }
 }
