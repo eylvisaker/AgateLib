dd4144db0649f9aca4733a1824e7fc3b758a5c3a
Erik Ylvisaker <eylvisaker@gmail.com>
2009-02-10 07:12:11 +0000

    Added TgzFileProvider and tester application.

---
+++ Tests/Core/TgzProvider/TgzProviderTester.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AgateLib;
+using AgateLib.DisplayLib;
+using AgateLib.Geometry;
+using AgateLib.Utility;
+
+namespace TgzProviderTester
+{
+    class TgzProviderTester
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            new TgzProviderTester().Run();
+        }
+
+        private void Run()
+        {
+            var tgz = new TgzFileProvider("archives/dogs.tar.gz");
+
+            // These two lines are used by AgateLib tests to locate
+            // driver plugins and images.
+            AgateFileProvider.Assemblies.AddPath("../Drivers");
+            AgateFileProvider.Images.Add(tgz);
+
+            using (AgateSetup setup = new AgateSetup())
+            {
+                setup.AskUser = true;
+                setup.Initialize(true, false, false);
+                if (setup.WasCanceled)
+                    return;
+
+                DisplayWindow wind = new DisplayWindow(CreateWindowParams.Windowed(
+                    "TgzFileProvider Tester", 800, 600, null, false));
+
+                Surface surf = new Surface("dogs.png");
+                PixelBuffer pix = surf.ReadPixels();
+
+                while (wind.IsClosed == false)
+                {
+                    Display.BeginFrame();
+                    Display.Clear(Color.Blue);
+                    surf.Draw();
+
+                    Display.EndFrame();
+                    Core.KeepAlive();
+                }
+            }
+        }
+    }
+}
