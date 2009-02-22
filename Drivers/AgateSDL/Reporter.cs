111c7c31efc59d08020f429833cb324f83553f2d
Erik Ylvisaker <eylvisaker@gmail.com>
2009-02-22 01:32:08 +0000

    Organize directories.

--- Drivers/AgateSDL/Reporter.cs
+++ Drivers/AgateSDL/Reporter.cs
@@ -5,14 +5,23 @@ using AgateLib.Drivers;
 
 namespace AgateSDL
 {
+    using Audio;
+    using Input;
+
     class Reporter : AgateDriverReporter 
     {
 
         public override IEnumerable<AgateDriverInfo> ReportDrivers()
         {
             yield return new AgateDriverInfo(
+                AudioTypeID.SDL,
+                typeof(SDL_Audio),
+                "SDL with SDL_mixer",
+                300);
+
+            yield return new AgateDriverInfo(
                 InputTypeID.SDL,
-                typeof(Input_SDL),
+                typeof(SDL_Input),
                 "SDL Input",
                 300);
         }
