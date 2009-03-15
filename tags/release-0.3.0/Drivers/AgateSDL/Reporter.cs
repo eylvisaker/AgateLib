478a83f4dde93daee620649c397382eefdeb018c
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-03-15 04:31:24 +0000

    Tag 0.3.0 release.

---
+++ tags/release-0.3.0/Drivers/AgateSDL/Reporter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgateLib.Drivers;
+
+namespace AgateSDL
+{
+    using Audio;
+    using Input;
+
+    class Reporter : AgateDriverReporter 
+    {
+
+        public override IEnumerable<AgateDriverInfo> ReportDrivers()
+        {
+            yield return new AgateDriverInfo(
+                AudioTypeID.SDL,
+                typeof(SDL_Audio),
+                "SDL with SDL_mixer",
+                300);
+
+            yield return new AgateDriverInfo(
+                InputTypeID.SDL,
+                typeof(SDL_Input),
+                "SDL Input",
+                300);
+        }
+    }
+}
---
+++ tags/release-0.3.0/Drivers/OldCode/AgateWGL/README.txt
@@ -0,0 +1,6 @@
+AgateLib Windows OpenGL implementation.
+
+This driver was started to use OpenGL as a driver for Agate.  Unfortunately, I don't know much about OpenGL, and most of the tutorials which exist use code from the WGL library.  The main goal of developing an OpenGL driver as an alternative to the Direct3D driver is to have cross-platform support.  But WGL is a Windows-only library so this is a problem.  This type of driver may be a good starting place to using another library implemented in Tao, or something which specifically uses XGL, or something else.
+
+- ERY 8/31/06
+
