0220ddbc0be739cae67cbaea8f88be3bab1e31cd
Erik Ylvisaker <eylvisaker@gmail.com>
2009-03-16 16:39:58 +0000

    Add AppTester project.

---
+++ Tests/Core/AppTester/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AgateLib;
+
+namespace AppTester
+{
+    class App : AgateApplication  
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            new App().Run();
+        }
+
+        protected override AppInitParameters GetAppInitParameters()
+        {
+            var retval = base.GetAppInitParameters();
+
+            return retval;
+        }
+    }
+}
