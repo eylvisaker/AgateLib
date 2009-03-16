e08c62783b890e82b282f5789fda668bae45cc9f
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-03-16 14:17:44 +0000

    Add application framework.

---
+++ trunk/AgateLib/AppInitParameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib.Geometry;
+
+namespace AgateLib
+{
+    public class AppInitParameters
+    {
+        public AppInitParameters()
+        {
+            WindowSize = new Size(800, 600);
+            InitializeAudio = true;
+            InitializeDisplay = true;
+            InitializeJoysticks = true;
+        }
+
+        public Size WindowSize { get; set; }
+        public bool FullScreen { get; set; }
+        public bool ShowSplashScreen { get; set; }
+
+        public bool InitializeDisplay { get; set; }
+        public bool InitializeAudio { get; set; }
+        public bool InitializeJoysticks { get; set; }
+    }
+}
