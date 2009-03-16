343b6e0939effe602e4a387ff1b59d6f3ddd06c0
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-03-16 16:19:10 +0000

    Include image resources in AgateLib.

---
+++ trunk/AgateLib/InternalResources/Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib.DisplayLib;
+using AgateLib.Utility;
+
+namespace AgateLib.InternalResources
+{
+    internal static class Data
+    {
+        static TgzFileProvider mProvider = new TgzFileProvider("images", DataResources.images);
+
+        static Surface mPoweredBy;
+
+        internal static Surface PoweredBy
+        {
+            get
+            {
+                if (mPoweredBy != null)
+                    return mPoweredBy;
+
+                mPoweredBy = new Surface(mProvider, "agate-powered.png");
+
+                return mPoweredBy;
+            }
+        }
+    }
+}
