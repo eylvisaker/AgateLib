f094801386f91dcb10fc89f18b383e937ece3e77
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-03-23 07:59:10 +0000

    Add in TextLayout test application.

--- branches/font/Tests/Fonts/Fonts/Fonts.cs
+++ branches/font/Tests/Fonts/Fonts/Fonts.cs
@@ -76,9 +76,7 @@ namespace FontTester
                     font.DrawText(textPoint, text);
                     font.SetScale(1.0, 1.0);
 
-                    font.Color = Color.Red;
-                    font.DrawText(50, 280, "Test Layout {0} Text\nTest second line.", AgateLib.InternalResources.Data.PoweredBy);
-
+                    
                     // this draws a white background behind the text we want to Display.
                     text = "F2: Toggle VSync   F5:  Toggle Windowed / Fullscreen      ";
                     text += "FPS: " + Display.FramesPerSecond.ToString("0.00") + "    ";
---
+++ branches/font/Tests/Fonts/TextLayout/TextLayout.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AgateLib;
+using AgateLib.DisplayLib;
+using AgateLib.Geometry;
+
+namespace TextLayout
+{
+    class TextLayout : AgateApplication
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // These two lines are used by AgateLib tests to locate
+            // driver plugins and images.
+            AgateFileProvider.Assemblies.AddPath("../Drivers");
+            AgateFileProvider.Images.AddPath("Images");
+
+            new TextLayout().Run();
+        }
+
+        FontSurface font;
+        
+        protected override void Initialize()
+        {
+            font = new FontSurface("Arial", 10);
+            font.Color = Color.Black;
+        }
+
+        protected override void Render(double time_ms)
+        {
+            Display.Clear(Color.White);
+
+            font.TextImageLayout = TextImageLayout.InlineTop;
+            font.DrawText(0, 0, "Test InlineTop:\n{0}Test Layout {0} Text\nTest second line.",
+                AgateLib.InternalResources.Data.PoweredBy);
+
+            font.TextImageLayout = TextImageLayout.InlineCenter;
+            font.DrawText(0, 100, "Test InlineCenter:\n{0}Test Layout {0} Text\nTest second line.",
+                AgateLib.InternalResources.Data.PoweredBy);
+
+            font.TextImageLayout = TextImageLayout.InlineBottom;
+            font.DrawText(0, 200, "Test InlineBottom:\n{0}Test Layout {0} Text\nTest second line.",
+                AgateLib.InternalResources.Data.PoweredBy);
+
+
+        }
+    }
+}
