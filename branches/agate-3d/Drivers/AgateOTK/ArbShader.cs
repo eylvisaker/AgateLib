ee91aed2a1a99f3c106a8b3e378155a9d378ff1f
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-04-09 06:49:27 +0000

    Add missing ArbShader.cs file.

---
+++ branches/agate-3d/Drivers/AgateOTK/ArbShader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib.DisplayLib.Shaders;
+using OpenTK.Graphics;
+
+namespace AgateOTK
+{
+    class ArbShader : ShaderProgram
+    {
+        int programHandle;
+        Dictionary<string, int> mUniforms = new Dictionary<string, int>();
+
+        public ArbShader(int handle, GlslVertexProgram vert, GlslFragmentProgram frag)
+        {
+            programHandle = handle;
+            this.vertex = vert;
+            this.pixel = frag;
+        }
+
+        GlslVertexProgram vertex;
+        GlslFragmentProgram pixel;
+
+        public override PixelShader PixelShader
+        {
+            get { return pixel; }
+        }
+        public override VertexShader VertexShader
+        {
+            get { return vertex; }
+        }
+
+        public int Handle
+        {
+            get { return programHandle; }
+        }
+
+        private int GetUniformLocation(string name)
+        {
+            if (mUniforms.ContainsKey(name))
+                return mUniforms[name];
+
+            return GL.Arb.GetUniformLocation(programHandle, name);
+        }
+
+        public override void SetUniform(string name, params float[] v)
+        {
+            int loc = GetUniformLocation(name);
+
+        }
+        public override void SetUniform(string name, params int[] v)
+        {
+        }
+
+        public override void SetUniform(string name, AgateLib.Geometry.Matrix4 matrix)
+        {
+        }
+    }
+}
