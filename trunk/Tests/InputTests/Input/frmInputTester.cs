64ec87719a6c5555a29d521329c9e6ffdef50015
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-04-12 17:46:57 +0000

    Fix input tester to not crash when no joysticks are connected.

--- trunk/Tests/InputTests/Input/frmInputTester.cs
+++ trunk/Tests/InputTests/Input/frmInputTester.cs
@@ -15,124 +15,124 @@ using AgateLib.InputLib;
 
 namespace Tests.InputTester
 {
-    public partial class Form1 : Form
-    {
-        
-        public Form1()
-        {
-            InitializeComponent();
+	public partial class Form1 : Form
+	{
 
-            Keyboard.KeyDown += new InputEventHandler(Keyboard_KeyDown);
-            Keyboard.KeyUp += new InputEventHandler(Keyboard_KeyUp);
+		public Form1()
+		{
+			InitializeComponent();
 
-            Mouse.MouseWheel += new InputEventHandler(Mouse_MouseWheel);
-            Mouse.MouseMove += new InputEventHandler(Mouse_MouseMove);
-            Mouse.MouseDown += new InputEventHandler(Mouse_MouseDown);
-            Mouse.MouseUp += new InputEventHandler(Mouse_MouseUp);
-            Mouse.MouseDoubleClick += new InputEventHandler(Mouse_MouseDoubleClickEvent);
+			Keyboard.KeyDown += new InputEventHandler(Keyboard_KeyDown);
+			Keyboard.KeyUp += new InputEventHandler(Keyboard_KeyUp);
 
-            new DisplayWindow(CreateWindowParams.FromControl(agateRenderTarget1));
+			Mouse.MouseWheel += new InputEventHandler(Mouse_MouseWheel);
+			Mouse.MouseMove += new InputEventHandler(Mouse_MouseMove);
+			Mouse.MouseDown += new InputEventHandler(Mouse_MouseDown);
+			Mouse.MouseUp += new InputEventHandler(Mouse_MouseUp);
+			Mouse.MouseDoubleClick += new InputEventHandler(Mouse_MouseDoubleClickEvent);
 
-            Application.Idle += new EventHandler(Application_Idle);
-        }
+			new DisplayWindow(CreateWindowParams.FromControl(agateRenderTarget1));
 
-        void Application_Idle(object sender, EventArgs e)
-        {
-            while (this.Visible)
-            {
-                numericUpDown1.Maximum = JoystickInput.Joysticks.Count - 1;
-                Joystick j = JoystickInput.Joysticks[(int)numericUpDown1.Value];
+			Application.Idle += new EventHandler(Application_Idle);
+		}
 
-                StringBuilder b = new StringBuilder();
-                b.AppendLine(j.Name);
-                b.Append("Axis Count: ");
-                b.AppendLine(j.AxisCount.ToString());
+		void Application_Idle(object sender, EventArgs e)
+		{
+			numericUpDown1.Maximum = JoystickInput.Joysticks.Count - 1;
+			if (JoystickInput.Joysticks.Count > 0)
+			{
+				Joystick j = JoystickInput.Joysticks[(int)numericUpDown1.Value];
 
-                for (int i = 0; i < j.AxisCount; i++)
-                {
-                    b.Append("Axis ");
-                    b.Append(i.ToString());
-                    b.Append(": ");
-                    b.Append(j.GetAxisValue(i).ToString());
-                    b.AppendLine();
-                }
+				StringBuilder b = new StringBuilder();
+				b.AppendLine(j.Name);
+				b.Append("Axis Count: ");
+				b.AppendLine(j.AxisCount.ToString());
 
-                b.AppendLine();
+				for (int i = 0; i < j.AxisCount; i++)
+				{
+					b.Append("Axis ");
+					b.Append(i.ToString());
+					b.Append(": ");
+					b.Append(j.GetAxisValue(i).ToString());
+					b.AppendLine();
+				}
 
-                b.Append("X: ");
-                b.AppendLine(j.Xaxis.ToString());
-                b.Append("Y: ");
-                b.AppendLine(j.Yaxis.ToString());
-                b.AppendLine();
+				b.AppendLine();
 
-                b.Append("Buttons: ");
+				b.Append("X: ");
+				b.AppendLine(j.Xaxis.ToString());
+				b.Append("Y: ");
+				b.AppendLine(j.Yaxis.ToString());
+				b.AppendLine();
 
-                for (int i = 0; i < j.ButtonCount; i++)
-                {
-                    if (j.GetButtonState(i))
-                        b.Append(i.ToString());
-                }
+				b.Append("Buttons: ");
+
+				for (int i = 0; i < j.ButtonCount; i++)
+				{
+					if (j.GetButtonState(i))
+						b.Append(i.ToString());
+				}
 
 
-                lblJoystick.Text = b.ToString();
+				lblJoystick.Text = b.ToString();
+			}
 
-                Core.KeepAlive();
-            }
-        }
+			Core.KeepAlive();
+		}
 
-        void Mouse_MouseDoubleClickEvent(InputEventArgs e)
-        {
-            lblMouseButton.Text = "Mouse Double Click " + e.MouseButtons.ToString();
-        }
-        void Mouse_MouseUp(InputEventArgs e)
-        {
-            lblMouseButton.Text = "Mouse Button Up " + e.MouseButtons.ToString();   
-        }
-        void Mouse_MouseDown(InputEventArgs e)
-        {
-            lblMouseButton.Text = "Mouse Button Down " + e.MouseButtons.ToString();
-        }
-        void Mouse_MouseMove(InputEventArgs e)
-        {
-            lblMouseMove.Text = "Mouse Moved " + e.MousePosition.ToString();
-        }
-        void Mouse_MouseWheel(InputEventArgs e)
-        {
-            lblMouseButton.Text = "Mouse Wheel " + e.WheelDelta.ToString();
-        }
+		void Mouse_MouseDoubleClickEvent(InputEventArgs e)
+		{
+			lblMouseButton.Text = "Mouse Double Click " + e.MouseButtons.ToString();
+		}
+		void Mouse_MouseUp(InputEventArgs e)
+		{
+			lblMouseButton.Text = "Mouse Button Up " + e.MouseButtons.ToString();
+		}
+		void Mouse_MouseDown(InputEventArgs e)
+		{
+			lblMouseButton.Text = "Mouse Button Down " + e.MouseButtons.ToString();
+		}
+		void Mouse_MouseMove(InputEventArgs e)
+		{
+			lblMouseMove.Text = "Mouse Moved " + e.MousePosition.ToString();
+		}
+		void Mouse_MouseWheel(InputEventArgs e)
+		{
+			lblMouseButton.Text = "Mouse Wheel " + e.WheelDelta.ToString();
+		}
 
-        void Keyboard_KeyUp(InputEventArgs e)
-        {
-            this.lblKeyPress.Text = "Released key " + e.KeyCode;
-        }
-        void Keyboard_KeyDown(InputEventArgs e)
-        {
-            this.lblKeyPress.Text = "Pressed key " + e.KeyCode;
-            this.lblKeyString.Text = "Pressed key string [" + e.KeyString + "]";
-        }
+		void Keyboard_KeyUp(InputEventArgs e)
+		{
+			this.lblKeyPress.Text = "Released key " + e.KeyCode;
+		}
+		void Keyboard_KeyDown(InputEventArgs e)
+		{
+			this.lblKeyPress.Text = "Pressed key " + e.KeyCode;
+			this.lblKeyString.Text = "Pressed key string [" + e.KeyString + "]";
+		}
 
-        protected override void OnFormClosed(FormClosedEventArgs e)
-        {
-            base.OnFormClosed(e);
-            Display.Dispose();
-        }
+		protected override void OnFormClosed(FormClosedEventArgs e)
+		{
+			base.OnFormClosed(e);
+			Display.Dispose();
+		}
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            lblKeyEvent.Text = "KeyCode: " + e.KeyCode.ToString() + "\r\n" +
-                "KeyData: " + e.KeyData.ToString() + "\r\n" +
-                "KeyValue: " + e.KeyValue.ToString() + "\r\n" +
-                "KeyValueChar: " + ((char)e.KeyValue).ToString();
-        }
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			lblKeyEvent.Text = "KeyCode: " + e.KeyCode.ToString() + "\r\n" +
+				"KeyData: " + e.KeyData.ToString() + "\r\n" +
+				"KeyValue: " + e.KeyValue.ToString() + "\r\n" +
+				"KeyValueChar: " + ((char)e.KeyValue).ToString();
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Keyboard.ReleaseAllKeys();
-        }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Keyboard.ReleaseAllKeys();
+		}
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
