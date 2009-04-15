ccd2d725ae39a5ac33c64f5b9c88773a2afbcdd3
Erik Ylvisaker <eylvisaker@gmail.com>
2009-04-15 02:27:40 +0000

    Make packed sprite creator compile.

--- Tools/PackedSpriteCreator/SpriteEditor.cs
+++ Tools/PackedSpriteCreator/SpriteEditor.cs
@@ -11,297 +11,297 @@ using AgateLib.Sprites;
 
 namespace PackedSpriteCreator
 {
-    public partial class SpriteEditor : UserControl
-    {
-        AgateResourceCollection mResources;
-        SpriteResource mCurrentSprite;
-        SpriteRenderer drawer = new SpriteRenderer();
-        AgateLib.DisplayLib.DisplayWindow wind;
-        Sprite mAgateSprite;
+	public partial class SpriteEditor : UserControl
+	{
+		AgateResourceCollection mResources;
+		SpriteResource mCurrentSprite;
+		SpriteRenderer drawer = new SpriteRenderer();
+		AgateLib.DisplayLib.DisplayWindow wind;
+		Sprite mAgateSprite;
 
-        public SpriteEditor()
-        {
-            InitializeComponent();
-        }
+		public SpriteEditor()
+		{
+			InitializeComponent();
+		}
 
-        public AgateResourceCollection Resources
-        {
-            get { return mResources; }
-            set
-            {
-                mResources = value;
+		public AgateResourceCollection Resources
+		{
+			get { return mResources; }
+			set
+			{
+				mResources = value;
 
-                OnResourcesChanged();
-            }
-        }
+				OnResourcesChanged();
+			}
+		}
 
-        private void OnResourcesChanged()
-        {
-            lstSprites.Items.Clear();
+		private void OnResourcesChanged()
+		{
+			lstSprites.Items.Clear();
 
-            if (mResources == null)
-            {
-                lstSprites.Enabled = false;
-                return;
-            }
+			if (mResources == null)
+			{
+				lstSprites.Enabled = false;
+				return;
+			}
 
-            lstSprites.Enabled = true;
-            lstSprites.Items.AddRange(mResources.Sprites.ToArray());
-            
-        }
+			lstSprites.Enabled = true;
+			lstSprites.Items.AddRange(mResources.Sprites.ToArray());
 
-        bool changingSprite = false;
+		}
 
-        private void lstSprites_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            mCurrentSprite = lstSprites.SelectedItem as SpriteResource;
+		bool changingSprite = false;
 
-            if (mCurrentSprite == null)
-            {
-                propertiesPanel.Visible = false;
-                return;
-            }
+		private void lstSprites_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			mCurrentSprite = lstSprites.SelectedItem as SpriteResource;
 
-            propertiesPanel.Visible = true;
+			if (mCurrentSprite == null)
+			{
+				propertiesPanel.Visible = false;
+				return;
+			}
 
-            try
-            {
-                changingSprite = true;
-                UpdateSprite();
+			propertiesPanel.Visible = true;
 
-                txtName.Text = mCurrentSprite.Name;
-                nudTimePerFrame.Value = (decimal)mCurrentSprite.TimePerFrame;
+			try
+			{
+				changingSprite = true;
+				UpdateSprite();
 
-                FillFrameList(-1);
-            }
-            finally
-            {
-                changingSprite = false;
-            }
-        }
+				txtName.Text = mCurrentSprite.Name;
+				nudTimePerFrame.Value = (decimal)mCurrentSprite.TimePerFrame;
 
-        private void UpdateSprite()
-        {
-            try
-            {
-                UpdateSprite(new Sprite(Resources, mCurrentSprite.Name));
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(this, string.Format(
-                    "Failed to load sprite {0}." + Environment.NewLine + "{1}", mCurrentSprite.Name,
-                    e.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-        private void UpdateSprite(Sprite updatedSprite)
-        {
-            if (mAgateSprite != null)
-                mAgateSprite.Dispose();
+				FillFrameList(-1);
+			}
+			finally
+			{
+				changingSprite = false;
+			}
+		}
 
-            mAgateSprite = updatedSprite;
-            mAgateSprite.IsAnimating = chkAnimating.Checked;
-        }
+		private void UpdateSprite()
+		{
+			try
+			{
+				UpdateSprite(new Sprite(Resources, mCurrentSprite.Name));
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(this, string.Format(
+					"Failed to load sprite {0}." + Environment.NewLine + "{1}", mCurrentSprite.Name,
+					e.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		private void UpdateSprite(Sprite updatedSprite)
+		{
+			if (mAgateSprite != null)
+				mAgateSprite.Dispose();
 
-        private void FillFrameList(int newSelection)
-        {
-            FillFrameList(new int[] { newSelection });
-        }
-        private void FillFrameList(IEnumerable<int> selections)
-        {
-            lstFrames.Items.Clear();
-            lstFrames.Items.AddRange(mCurrentSprite.Frames.ToArray());
+			mAgateSprite = updatedSprite;
+			mAgateSprite.IsAnimating = chkAnimating.Checked;
+		}
 
-            lstFrames.SelectedIndices.Clear();
-            foreach (var index in selections)
-                lstFrames.SelectedIndices.Add(index);
+		private void FillFrameList(int newSelection)
+		{
+			FillFrameList(new int[] { newSelection });
+		}
+		private void FillFrameList(IEnumerable<int> selections)
+		{
+			lstFrames.Items.Clear();
+			lstFrames.Items.AddRange(mCurrentSprite.ChildElements.ToArray());
 
-            lstFrames_SelectedIndexChanged(this, EventArgs.Empty);
-        }
+			lstFrames.SelectedIndices.Clear();
+			foreach (var index in selections)
+				lstFrames.SelectedIndices.Add(index);
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            if (propertiesPanel.Visible == false)
-                return;
+			lstFrames_SelectedIndexChanged(this, EventArgs.Empty);
+		}
 
-            drawer.DrawSprite(wind, mAgateSprite); 
-        }
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (propertiesPanel.Visible == false)
+				return;
 
-        private void SpriteEditor_Load(object sender, EventArgs e)
-        {
-            if (DesignMode)
-                return;
-            if (Parent == null)
-                return;
+			drawer.DrawSprite(wind, mAgateSprite);
+		}
 
-            wind = new AgateLib.DisplayLib.DisplayWindow(AgateLib.DisplayLib.CreateWindowParams.FromControl(agateRenderTarget1));
+		private void SpriteEditor_Load(object sender, EventArgs e)
+		{
+			if (DesignMode)
+				return;
+			if (Parent == null)
+				return;
 
-        }
+			wind = new AgateLib.DisplayLib.DisplayWindow(AgateLib.DisplayLib.CreateWindowParams.FromControl(agateRenderTarget1));
 
-        private void txtName_Validating(object sender, CancelEventArgs e)
-        {
-            if (changingSprite) return;
+		}
 
-            if (Resources.Contains(txtName.Text) == false)
-                return;
+		private void txtName_Validating(object sender, CancelEventArgs e)
+		{
+			if (changingSprite) return;
 
-            if (Resources[txtName.Text] == mCurrentSprite)
-                return;
+			if (Resources.Contains(txtName.Text) == false)
+				return;
 
-            e.Cancel = true;
-        }
+			if (Resources[txtName.Text] == mCurrentSprite)
+				return;
 
-        private void txtName_Validated(object sender, EventArgs e)
-        {
-            if (changingSprite) return;
+			e.Cancel = true;
+		}
 
-            Resources.Remove(mCurrentSprite);
-            
-            mCurrentSprite.Name = txtName.Text;
+		private void txtName_Validated(object sender, EventArgs e)
+		{
+			if (changingSprite) return;
 
-            Resources.Add(mCurrentSprite);
+			Resources.Remove(mCurrentSprite);
 
-            int index = lstSprites.SelectedIndex;
-            lstSprites.Items.RemoveAt(index);
-            lstSprites.Items.Insert(index, mCurrentSprite);
-            lstSprites.SelectedIndex = index;
-        }
+			mCurrentSprite.Name = txtName.Text;
 
-        private void nudTimePerFrame_ValueChanged(object sender, EventArgs e)
-        {
-            if (changingSprite) return;
+			Resources.Add(mCurrentSprite);
 
-            mCurrentSprite.TimePerFrame = (double)nudTimePerFrame.Value;
-            mAgateSprite.TimePerFrame = (double)nudTimePerFrame.Value;
+			int index = lstSprites.SelectedIndex;
+			lstSprites.Items.RemoveAt(index);
+			lstSprites.Items.Insert(index, mCurrentSprite);
+			lstSprites.SelectedIndex = index;
+		}
 
-        }
+		private void nudTimePerFrame_ValueChanged(object sender, EventArgs e)
+		{
+			if (changingSprite) return;
 
-        private void lstFrames_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (lstFrames.SelectedIndex == -1)
-            {
-                btnDelete.Enabled = false;
-                btnMoveDown.Enabled = false;
-                btnMoveUp.Enabled = false;
-            }
-            else
-            {
-                btnMoveUp.Enabled = true;
-                btnMoveDown.Enabled = true;
-                btnDelete.Enabled = true;
+			mCurrentSprite.TimePerFrame = (double)nudTimePerFrame.Value;
+			mAgateSprite.TimePerFrame = (double)nudTimePerFrame.Value;
 
-                if (lstFrames.SelectedIndices.Contains(0))
-                {
-                    btnMoveUp.Enabled = false;
-                }
-                if (lstFrames.SelectedIndices.Contains(lstFrames.Items.Count-1))
-                {
-                    btnMoveDown.Enabled = false;
-                }
-                if (lstFrames.SelectedIndices.Count == 0)
-                    btnDelete.Enabled = false;
-            }
+		}
 
-            if (changingSprite)
-                return;
+		private void lstFrames_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (lstFrames.SelectedIndex == -1)
+			{
+				btnDelete.Enabled = false;
+				btnMoveDown.Enabled = false;
+				btnMoveUp.Enabled = false;
+			}
+			else
+			{
+				btnMoveUp.Enabled = true;
+				btnMoveDown.Enabled = true;
+				btnDelete.Enabled = true;
 
-            if (mAgateSprite != null)
-            {
-                if (chkAnimating.Checked == false)
-                    mAgateSprite.CurrentFrameIndex = lstFrames.SelectedIndex;
-            }
-        }
+				if (lstFrames.SelectedIndices.Contains(0))
+				{
+					btnMoveUp.Enabled = false;
+				}
+				if (lstFrames.SelectedIndices.Contains(lstFrames.Items.Count - 1))
+				{
+					btnMoveDown.Enabled = false;
+				}
+				if (lstFrames.SelectedIndices.Count == 0)
+					btnDelete.Enabled = false;
+			}
 
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            frmAddSpriteFrames frm = new frmAddSpriteFrames();
-            frm.SpriteSize = AgateLib.WinForms.Interop.Convert(mCurrentSprite.Size);
+			if (changingSprite)
+				return;
 
-            if (frm.ShowDialog(this) == DialogResult.OK)
-            {
+			if (mAgateSprite != null)
+			{
+				if (chkAnimating.Checked == false)
+					mAgateSprite.CurrentFrameIndex = lstFrames.SelectedIndex;
+			}
+		}
 
-            }
-        }
-        private void btnMoveUp_Click(object sender, EventArgs e)
-        {
-            List<int> selections = new List<int>();
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			frmAddSpriteFrames frm = new frmAddSpriteFrames();
+			frm.SpriteSize = AgateLib.WinForms.Interop.Convert(mCurrentSprite.Size);
 
-            foreach (int index in lstFrames.SelectedIndices)
-            {
-                var frame = mCurrentSprite.Frames[index];
-                mCurrentSprite.Frames.RemoveAt(index);
-                mCurrentSprite.Frames.Insert(index - 1, frame);
+			if (frm.ShowDialog(this) == DialogResult.OK)
+			{
 
-                selections.Add(index - 1);
-            }
+			}
+		}
+		private void btnMoveUp_Click(object sender, EventArgs e)
+		{
+			List<int> selections = new List<int>();
 
-            UpdateSprite();
-            FillFrameList(selections);
+			foreach (int index in lstFrames.SelectedIndices)
+			{
+				var frame = mCurrentSprite.ChildElements[index];
+				mCurrentSprite.ChildElements.RemoveAt(index);
+				mCurrentSprite.ChildElements.Insert(index - 1, frame);
 
-        }
-        private void btnMoveDown_Click(object sender, EventArgs e)
-        {
-            List<int> selections = new List<int>();
+				selections.Add(index - 1);
+			}
 
-            foreach (int index in lstFrames.SelectedIndices)
-            {
-                var frame = mCurrentSprite.Frames[index];
-                mCurrentSprite.Frames.RemoveAt(index);
-                mCurrentSprite.Frames.Insert(index + 1, frame);
+			UpdateSprite();
+			FillFrameList(selections);
 
-                selections.Add(index + 1);
-            }
+		}
+		private void btnMoveDown_Click(object sender, EventArgs e)
+		{
+			List<int> selections = new List<int>();
 
-            UpdateSprite();
-            FillFrameList(selections);
+			foreach (int index in lstFrames.SelectedIndices)
+			{
+				var frame = mCurrentSprite.ChildElements[index];
+				mCurrentSprite.ChildElements.RemoveAt(index);
+				mCurrentSprite.ChildElements.Insert(index + 1, frame);
 
-        }
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            List<SpriteResource.SpriteFrameResource> framesToDelete = new List<SpriteResource.SpriteFrameResource>();
+				selections.Add(index + 1);
+			}
 
-            foreach (int index in lstFrames.SelectedIndices)
-            {
-                framesToDelete.Add(mCurrentSprite.Frames[index]);
-            }
+			UpdateSprite();
+			FillFrameList(selections);
 
-            foreach(SpriteResource.SpriteFrameResource frame in framesToDelete)
-            {
-                mCurrentSprite.Frames.Remove(frame);
-            }
+		}
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			var framesToDelete = new List<SpriteResource.SpriteSubResource>();
 
-            UpdateSprite();
-            FillFrameList(-1);
-        }
+			foreach (int index in lstFrames.SelectedIndices)
+			{
+				framesToDelete.Add(mCurrentSprite.ChildElements[index]);
+			}
+
+			foreach (SpriteResource.SpriteFrameResource frame in framesToDelete)
+			{
+				mCurrentSprite.ChildElements.Remove(frame);
+			}
+
+			UpdateSprite();
+			FillFrameList(-1);
+		}
 
 
-        private void chkAnimating_CheckedChanged(object sender, EventArgs e)
-        {
-            if (mAgateSprite == null)
-                return;
+		private void chkAnimating_CheckedChanged(object sender, EventArgs e)
+		{
+			if (mAgateSprite == null)
+				return;
 
-            mAgateSprite.IsAnimating = chkAnimating.Checked;
-        }
+			mAgateSprite.IsAnimating = chkAnimating.Checked;
+		}
 
-        private void btnNewSprite_Click(object sender, EventArgs e)
-        {
-            frmNewSprite frm = new frmNewSprite();
+		private void btnNewSprite_Click(object sender, EventArgs e)
+		{
+			frmNewSprite frm = new frmNewSprite();
 
-            if (frm.ShowDialog(this, Resources) == DialogResult.Cancel)
-                return;
+			if (frm.ShowDialog(this, Resources) == DialogResult.Cancel)
+				return;
 
-            frmAddSpriteFrames frmAdd = new frmAddSpriteFrames();
-            frmAdd.SpriteSize = frm.SpriteFrameSize;
+			frmAddSpriteFrames frmAdd = new frmAddSpriteFrames();
+			frmAdd.SpriteSize = frm.SpriteFrameSize;
 
-            if (frmAdd.ShowDialog(this) == DialogResult.Cancel)
-                return;
+			if (frmAdd.ShowDialog(this) == DialogResult.Cancel)
+				return;
 
-            SpriteResource res = new SpriteResource(frm.SpriteName);
+			SpriteResource res = new SpriteResource(frm.SpriteName);
 
-            
 
-            Resources.Add(res);
 
-            OnResourcesChanged();
-        }
-    }
+			Resources.Add(res);
+
+			OnResourcesChanged();
+		}
+	}
 }
