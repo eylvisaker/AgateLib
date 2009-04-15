a82c37bb1fc1420aee101ce434ca271940a426da
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-04-15 06:53:54 +0000

    Rename FontSurfaceState to FontState. Add missing Cache/FontStateCache.cs  from previous commit.

---
+++ branches/font/AgateLib/DisplayLib/Cache/FontStateCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.DisplayLib.Cache
+{
+	public abstract class FontStateCache
+	{
+		protected internal abstract FontStateCache Clone();
+
+		protected internal virtual void OnTextChanged(FontState fontState)
+		{
+		}
+		protected internal virtual void OnLocationChanged(FontState fontState)
+		{
+		}
+		protected internal virtual void OnDisplayAlignmentChanged(FontState fontState)
+		{
+		}
+		protected internal virtual void OnColorChanged(FontState fontState)
+		{
+		}
+		protected internal virtual void OnScaleChanged(FontState fontState)
+		{
+		}
+	}
+}
