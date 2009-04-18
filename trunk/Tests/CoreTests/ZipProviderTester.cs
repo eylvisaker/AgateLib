32f9bf254ba127703fce530d8c282eca1e0a9bb8
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-04-18 22:28:00 +0000

    Fix title of ZipProviderTester.cs

--- trunk/Tests/CoreTests/ZipProviderTester.cs
+++ trunk/Tests/CoreTests/ZipProviderTester.cs
@@ -9,48 +9,48 @@ using AgateLib.Utility;
 
 namespace Tests.CoreTests
 {
-    class ZipProviderTester : IAgateTest
-    {
-        public void Main(string[] args)
-        {
-            var zip = new ZipFileProvider("Data/dogs.zip");
+	class ZipProviderTester : IAgateTest
+	{
+		public void Main(string[] args)
+		{
+			var zip = new ZipFileProvider("Data/dogs.zip");
 
-            using (AgateSetup setup = new AgateSetup())
-            {
-                setup.AskUser = true;
-                setup.Initialize(true, false, false);
-                if (setup.WasCanceled)
-                    return;
+			using (AgateSetup setup = new AgateSetup())
+			{
+				setup.AskUser = true;
+				setup.Initialize(true, false, false);
+				if (setup.WasCanceled)
+					return;
 
-                DisplayWindow wind = DisplayWindow.CreateWindowed(
-                    "TgzFileProvider Tester", 800, 600, false);
+				DisplayWindow wind = DisplayWindow.CreateWindowed(
+					"ZipFileProvider Tester", 800, 600, false);
 
-                Surface surf = new Surface(zip, "dogs.png");
-                Surface surf2 = new Surface(zip, "bigpaddle.png");
-                Surface surf3 = new Surface(zip, "other/bg-bricks.png");
+				Surface surf = new Surface(zip, "dogs.png");
+				Surface surf2 = new Surface(zip, "bigpaddle.png");
+				Surface surf3 = new Surface(zip, "other/bg-bricks.png");
 
-                PixelBuffer pix = surf.ReadPixels();
+				PixelBuffer pix = surf.ReadPixels();
 
-                while (wind.IsClosed == false)
-                {
-                    Display.BeginFrame();
-                    Display.Clear(Color.Blue);
+				while (wind.IsClosed == false)
+				{
+					Display.BeginFrame();
+					Display.Clear(Color.Blue);
 
-                    surf.Draw();
-                    surf2.Draw(10, 490);
-                    surf3.Draw(100, 100);
+					surf.Draw();
+					surf2.Draw(10, 490);
+					surf3.Draw(100, 100);
 
-                    Display.EndFrame();
-                    Core.KeepAlive();
-                }
-            }
-        }
+					Display.EndFrame();
+					Core.KeepAlive();
+				}
+			}
+		}
 
-        #region IAgateTest Members
+		#region IAgateTest Members
 
-        public string Name { get { return "Zip Provider"; } }
-        public string Category { get { return "Core"; } }
+		public string Name { get { return "Zip Provider"; } }
+		public string Category { get { return "Core"; } }
 
-        #endregion
-    }
+		#endregion
+	}
 }
