490d9671c083aaaeebbf0d8774185122ba7d8e35
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-05-11 06:04:11 +0000

    Initial commit of AgateSDX.

---
+++ branches/agate3d-3.2/Drivers/AgateSDX/HlslCompiler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib.DisplayLib.Shaders;
+using AgateLib.ImplementationBase;
+using Direct3D = SlimDX.Direct3D9;
+
+namespace AgateMDX
+{
+	class HlslCompiler : ShaderCompilerImpl 
+	{
+		SDX_Display mDisplay;
+
+		public HlslCompiler(SDX_Display display)
+		{
+			mDisplay = display;
+		}
+
+		public override ShaderProgram CompileEffect(ShaderLanguage language, string effectSource)
+		{
+			throw new NotImplementedException();
+		}
+		//public override ShaderProgram CompileEffect(ShaderLanguage language, string effectSource)
+		//{
+		//    Direct3D.Effect effect = Direct3D.Effect.FromString(mDisplay.D3D_Device.Device,
+		//        effectSource, null, null, Direct3D.ShaderFlags.None, null);
+
+		//    return new HlslShaderProgram(effect);
+		//}
+		public override ShaderProgram CompileShader(ShaderLanguage language, string vertexShaderSource, string pixelShaderSource)
+		{
+			throw new NotImplementedException();
+
+			//var vertexShaderStream = Direct3D.ShaderLoader.CompileShader(
+			//    vertexShaderSource, "main", null, "vs_1_1", Direct3D.ShaderFlags.None);
+
+			
+			//var vertexShader = new Direct3D.VertexShader(mDisplay.D3D_Device.Device, vertexShaderStream);
+
+
+			//var pixelShaderStream = Direct3D.ShaderLoader.CompileShader(
+			//    pixelShaderSource, "main", null, "ps_1_1", Direct3D.ShaderFlags.None);
+
+			//var pixelShader = new Direct3D.PixelShader(mDisplay.D3D_Device.Device, pixelShaderStream);
+
+			//return new HlslShaderProgram(vertexShader, pixelShader);
+		}
+	}
+}
---
+++ branches/agate3d-3.2/Drivers/AgateSDX/HlslShaderProgram.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib.DisplayLib;
+using AgateLib.DisplayLib.Shaders;
+using Direct3D = SlimDX.Direct3D9;
+
+namespace AgateMDX
+{
+	class HlslShaderProgram : ShaderProgram 
+	{
+		Direct3D.VertexShader mVertexShader;
+		Direct3D.PixelShader mPixelShader;
+		SDX_Display mDisplay;
+
+		public HlslShaderProgram(Direct3D.VertexShader vert, Direct3D.PixelShader pix)
+		{
+			mDisplay = (SDX_Display)Display.Impl;
+
+			mVertexShader = vert;
+			mPixelShader = pix;
+		}
+		public override PixelShader PixelShader
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override void SetUniform(string name, AgateLib.Geometry.Matrix4 matrix)
+		{
+			
+		}
+
+		public override void SetUniform(string name, params int[] v)
+		{
+			
+		}
+
+		public override void SetUniform(string name, params float[] v)
+		{
+			
+		}
+
+		public override VertexShader VertexShader
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public Direct3D.VertexShader HlslVertexShader
+		{
+			get { return mVertexShader; }
+		}
+		public Direct3D.PixelShader HlslPixelShader
+		{
+			get { return mPixelShader; }
+		}
+
+		public override void Render(RenderHandler handler, object obj)
+		{
+			mDisplay.D3D_Device.Device.VertexShader = mVertexShader;
+			mDisplay.D3D_Device.Device.PixelShader = mPixelShader;
+
+			handler(obj);
+			//int passcount = mEffect.Begin(SlimDX.Direct3D9.FX.None);
+
+			//for (int i = 0; i < passcount; i++)
+			//{
+			//    mEffect.BeginPass(i);
+			//    handler(obj);
+			//    mEffect.EndPass();
+			//}
+
+			//mEffect.End();
+		}
+	}
+
+	class HlslPixelShader : PixelShader
+	{
+	}
+
+	class HlslVertexShader : VertexShader
+	{ }
+}
