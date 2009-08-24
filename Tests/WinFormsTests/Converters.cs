e93bb4272e7593ab7e5a823787a3f7b2a29f0199
Erik Ylvisaker <eylvisaker@gmail.com>
2009-08-24 04:27:39 +0000

    Add test for winforms converters.

---
+++ Tests/WinFormsTests/Converters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib;
+using AgateLib.DisplayLib;
+using AgateLib.Geometry;
+using AgateLib.WinForms;
+
+namespace Tests.WinFormsTests
+{
+	class Converters : AgateApplication, IAgateTest 
+	{
+		public string Name
+		{
+			get { return "Conversion Tests"; }
+		}
+
+		public string Category
+		{
+			get { return "WinForms"; }
+		}
+
+		public void Main(string[] args)
+		{
+			Run(args);
+		}
+
+		protected override void Initialize()
+		{
+			Surface surf = new Surface("attacke.png");
+
+			System.Drawing.Bitmap bmp = surf.ToBitmap();
+
+			bmp.Save("test.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+		}
+	}
+}
