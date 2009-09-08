b928a06565ccdc293f9cd925ab858c02ee0b87ea
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-09-08 22:05:56 +0000

    Make sure to create a depth stencil surface for each render target.

--- trunk/Tests/DisplayTests/MultipleWindows/MultipleRenderTargetExample.Designer.cs
+++ trunk/Tests/DisplayTests/MultipleWindows/MultipleRenderTargetExample.Designer.cs
@@ -48,6 +48,9 @@ namespace Tests.MultipleWindows
 			// 
 			// pictureBox1
 			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
 			this.pictureBox1.Location = new System.Drawing.Point(12, 38);
 			this.pictureBox1.Name = "pictureBox1";
 			this.pictureBox1.Size = new System.Drawing.Size(136, 32);
