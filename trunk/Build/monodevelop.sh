af057b5b9c24aa32c71963c7d42b14a268531c6a
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-11-05 22:17:39 +0000

    Merge build branch and dump Prebuild based build system.

--- trunk/Build/monodevelop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mono Prebuild/Prebuild.exe /target monodev /file AgateLib.xml 
-mono Prebuild/Prebuild.exe /target monodev /file AgateTools.xml
-
--- trunk/Build/monodevelop2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mono Prebuild/Prebuild.exe /target vs2008 /file AgateLib.xml 
-mono Prebuild/Prebuild.exe /target vs2008 /file AgateTools.xml
