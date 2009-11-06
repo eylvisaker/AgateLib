c5055554ca62437fb62ea40daf3bffc88c661493
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-11-06 01:19:05 +0000

    Add PlatformDetection tester.

---
+++ branches/shaders/Tests/CoreTests/PlatformDetection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tests.CoreTests
+{
+	public partial class PlatformDetection : Form, IAgateTest 
+	{
+		public PlatformDetection()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+
+		}
+
+		#region IAgateTest Members
+
+		string IAgateTest.Name
+		{
+			get { return "Platform Detection"; }
+		}
+
+		string IAgateTest.Category
+		{
+			get { return "Core"; }
+		}
+
+		void IAgateTest.Main(string[] args)
+		{
+			Application.Run(this);
+		}
+
+		#endregion
+	}
+}
