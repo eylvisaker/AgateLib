74a9baa3aab880e163ac0d13281d5d5bbd438600
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-11-24 16:44:21 +0000

    Separate legacy GL code from GL_Display.

---
+++ trunk/Drivers/AgateOTK/PrimitiveRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgateLib.Geometry;
+
+namespace AgateOTK
+{
+	abstract class PrimitiveRenderer
+	{
+		public abstract void DrawLine(Point a, Point b, Color color);
+
+		public abstract void DrawRect(RectangleF rect, Color color);
+		public abstract void FillRect(RectangleF rect, Color color);
+		public abstract void FillRect(RectangleF rect, Gradient color);
+
+		public abstract void FillPolygon(PointF[] pts, Color color);
+
+	}
+}
