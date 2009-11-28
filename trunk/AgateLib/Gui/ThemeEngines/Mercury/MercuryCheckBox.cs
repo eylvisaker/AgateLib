5f29f4200d455e6346123a71ac5d5b6e8da5fe80
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-11-28 02:56:58 +0000

    Separate files for MercuryScheme.

---
+++ trunk/AgateLib/Gui/ThemeEngines/Mercury/MercuryCheckBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.Gui.ThemeEngines.Mercury
+{
+	public class MercuryCheckBox : MercurySchemeCommon
+	{
+		public Surface Image { get; set; }
+		public Surface Disabled { get; set; }
+		public Surface Check { get; set; }
+		public Surface Hover { get; set; }
+		public Surface Focus { get; set; }
+		public int Spacing { get; set; }
+		public int Margin { get; set; }
+
+		public MercuryCheckBox()
+		{
+			Spacing = 5;
+			Margin = 2;
+		}
+	}
+}
---
+++ trunk/AgateLib/Gui/ThemeEngines/Mercury/MercuryTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.Gui.ThemeEngines.Mercury
+{
+	public class MercuryTextBox : MercurySchemeCommon
+	{
+		public Surface Image { get; set; }
+		public Surface Disabled { get; set; }
+		public Surface Hover { get; set; }
+		public Surface Focus { get; set; }
+		public Rectangle StretchRegion { get; set; }
+		public int Margin { get; set; }
+
+		public MercuryTextBox()
+		{
+			Margin = 3;
+		}
+	}
+}
