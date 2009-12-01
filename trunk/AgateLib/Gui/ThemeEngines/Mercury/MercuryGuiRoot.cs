c4c483855dcafe8f3e7488485c5baf0f40ddee67
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2009-12-01 07:06:38 +0000

    Include missing files from last commit.

---
+++ trunk/AgateLib/Gui/ThemeEngines/Mercury/MercuryGuiRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.Gui.ThemeEngines.Mercury
+{
+	public class MercuryGuiRoot : MercuryWidget
+	{
+		public MercuryGuiRoot(MercuryScheme scheme)
+			: base(scheme)
+		{ }
+
+		public override void DrawWidget(Widget w)
+		{
+		}
+	}
+}
