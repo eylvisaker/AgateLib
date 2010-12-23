c590810bc91374d0b9196b2839bc26a580c192ff
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2010-12-23 19:30:40 +0000

    Fix Prerendered test to actually dispose of AgateSetup when it exits.

--- trunk/Tests/DisplayTests/Prerendered.cs
+++ trunk/Tests/DisplayTests/Prerendered.cs
@@ -21,39 +21,41 @@ namespace Tests.DisplayTests
 
 		public void Main(string[] args)
 		{
-			AgateSetup AS = new AgateSetup();
-
-			AS.AskUser = true;
-			AS.Initialize(true, false, true);
-			if (AS.WasCanceled)
-				return;
-			
-			DisplayWindow MainWindow = DisplayWindow.CreateWindowed ("Test", 800, 600);
-			FrameBuffer myBuffer = new FrameBuffer(200,35);
-			FontSurface font = FontSurface.AgateSans10;
-
-			Display.RenderTarget = myBuffer;
-			Display.BeginFrame();
-
-			Display.Clear(Color.Blue);
-			font.Color = Color.Red;
-			font.DrawText(3,3 , "HELLO WORLD");
-		
-			Display.EndFrame();
-			Display.FlushDrawBuffer();
-			Display.RenderTarget = MainWindow.FrameBuffer;
-
-			
-			while (MainWindow.IsClosed == false)
+			using (AgateSetup setup = new AgateSetup())
 			{
+				setup.AskUser = true;
+				setup.Initialize(true, false, true);
+				if (setup.WasCanceled)
+					return;
+
+				DisplayWindow MainWindow = DisplayWindow.CreateWindowed("Test", 800, 600);
+				FrameBuffer myBuffer = new FrameBuffer(200, 35);
+				FontSurface font = FontSurface.AgateSans10;
+
+				Display.RenderTarget = myBuffer;
 				Display.BeginFrame();
-				Display.Clear(Color.Black);
-				
-				myBuffer.RenderTarget.Draw(35,35);
+
+				Display.Clear(Color.Blue);
+				font.Color = Color.Red;
+				font.DrawText(3, 3, "HELLO WORLD");
 
 				Display.EndFrame();
-								
-				Core.KeepAlive();
+				Display.FlushDrawBuffer();
+				Display.RenderTarget = MainWindow.FrameBuffer;
+
+
+				while (MainWindow.IsClosed == false)
+				{
+					Display.BeginFrame();
+					Display.Clear(Color.Black);
+
+					myBuffer.RenderTarget.Draw(35, 35);
+					font.DrawText(38, 73, "HELLO WORLD");
+
+					Display.EndFrame();
+
+					Core.KeepAlive();
+				}
 			}
 		}
 
