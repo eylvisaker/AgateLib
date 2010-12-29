ff2b77f71cf57786d54cead42bae5abe86916eb1
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2010-12-29 21:38:41 +0000

    Add text to fullscreen test.

--- trunk/Tests/DisplayTests/FullScreen.cs
+++ trunk/Tests/DisplayTests/FullScreen.cs
@@ -19,6 +19,8 @@ namespace Tests.DisplayTests
 			get { return "Display"; }
 		}
 
+		string text = "Press Esc or Tilde to exit." + Environment.NewLine + "Starting Text";
+
 		public void Main(string[] args)
 		{
 			using (AgateSetup setup = new AgateSetup(args))
@@ -30,17 +32,29 @@ namespace Tests.DisplayTests
 				DisplayWindow wind = DisplayWindow.CreateFullScreen("Hello World", 640, 480);
 				Surface mySurface = new Surface("jellybean.png");
 
+				Keyboard.KeyDown += new InputEventHandler(Keyboard_KeyDown);
+				FontSurface font = FontSurface.AgateSans14;
+
 				// Run the program while the window is open.
 				while (Display.CurrentWindow.IsClosed == false && 
-					Keyboard.Keys[KeyCode.Escape] == false)
+					Keyboard.Keys[KeyCode.Escape] == false && 
+					Keyboard.Keys[KeyCode.Tilde] == false)
 				{
 					Display.BeginFrame();
 					Display.Clear(Color.DarkGreen);
+
+					font.DrawText(text);
 					mySurface.Draw(Mouse.X, Mouse.Y);
+
 					Display.EndFrame();
 					Core.KeepAlive();
 				}
 			}
+		}
+
+		void Keyboard_KeyDown(InputEventArgs e)
+		{
+			text += e.KeyString;
 		}
 
 	}
