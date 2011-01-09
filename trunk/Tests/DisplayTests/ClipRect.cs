1de9f8226303be6a58dc7c10908592cd2ae62618
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2011-01-09 18:47:43 +0000

    Modify ClipRect.cs to expose bug with coordinate systems when using cliprects.

--- trunk/Tests/DisplayTests/ClipRect.cs
+++ trunk/Tests/DisplayTests/ClipRect.cs
@@ -8,7 +8,7 @@ using AgateLib.Geometry;
 
 namespace Tests.DisplayTests
 {
-	class ClipRect:IAgateTest
+	class ClipRect : IAgateTest
 	{
 		#region IAgateTest Members
 
@@ -37,15 +37,27 @@ namespace Tests.DisplayTests
 				Color[] colors = new Color[] { 
 					Color.Red, Color.Orange, Color.Yellow, Color.YellowGreen, Color.Green, Color.Turquoise, Color.Blue, Color.Violet, Color.Wheat, Color.White};
 
+				Surface surf = new Surface("Data/wallpaper.png");
+
 				while (wind.IsClosed == false)
 				{
 					Display.BeginFrame();
 					Display.Clear();
 
+					for (int i = 0; i < 10; i++)
+					{
+						for (int j = 0; j < 10; j++)
+						{
+							Display.SetClipRect(new Rectangle(5 + i * 32, 5 + j * 32, 30, 30));
+
+							surf.Draw();
+						}
+					}
+
 					int index = 0;
 					for (int i = 10; i < 100; i += 10)
 					{
-						Display.SetClipRect(new Rectangle(i, i, 310 - i * 2, 310 - i * 2));
+						Display.SetClipRect(new Rectangle(320 + i, i, 310 - i * 2, 310 - i * 2));
 						Display.FillRect(0,0,640,480, colors[index]);
 						index++;
 					}
