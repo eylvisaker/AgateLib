e33afe41e376c0d9b403eb0529f159532004550b
Erik Ylvisaker <eylvisaker@gmail.com>
2012-02-08 01:35:21 +0000

    Add missing inputlib files.

---
+++ AgateLib/InputLib/Delegates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.InputLib
+{
+	/// <summary>
+	/// Input Event handler event type.
+	/// </summary>
+	/// <param name="e"></param>
+	public delegate void InputEventHandler(InputEventArgs e);
+
+	public delegate void JoystickEventHandler(object sender, JoystickEventArgs e);
+}
---
+++ AgateLib/InputLib/JoystickEventArgs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.InputLib
+{
+	public enum JoystickEventType
+	{
+		Button,
+		Hat,
+	}
+
+	public class JoystickEventArgs : EventArgs
+	{
+		private JoystickEventType mEventType;
+		private int mIndex;
+
+		public JoystickEventArgs(JoystickEventType joystickEventType, int index)
+		{
+			// TODO: Complete member initialization
+			this.mEventType = joystickEventType;
+			this.mIndex = index;
+		}
+
+		public JoystickEventType JoystickEventType
+		{
+			get { return mEventType; }
+		}
+
+		public int Index
+		{
+			get { return mIndex; }
+		}
+
+	}
+}
