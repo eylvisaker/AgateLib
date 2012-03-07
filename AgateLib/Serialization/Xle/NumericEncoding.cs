0deea48d774f7ed34198d763da62a17d3496fe7e
Erik Ylvisaker <eylvisaker@gmail.com>
2012-03-07 19:17:43 +0000

    Add temporary NumericEncoding.cs file.

---
+++ AgateLib/Serialization/Xle/NumericEncoding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.Serialization.Xle
+{
+	public enum NumericEncoding
+	{
+		Base64,
+		Csv,
+	}
+}
