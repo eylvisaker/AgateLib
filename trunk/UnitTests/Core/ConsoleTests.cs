12c74988729fd247185df951b5bd17fbd94ba966
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2013-12-03 03:37:41 +0000

    Refactor AgateConsole and introduce a unit test for it.

---
+++ trunk/UnitTests/Core/ConsoleTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgateLib.InputLib;
+
+namespace AgateLib.UnitTests.Core
+{
+	[TestClass]
+	public class ConsoleTests
+	{
+		[TestMethod]
+		public void HelpCommand()
+		{
+			AgateConsole c = new AgateConsole();
+			int count = 0;
+
+			c.CommandProcessor.DescribeCommand += (cmd) => { count++; return string.Empty; };
+
+			c.ProcessKeys("help");
+			c.ProcessKeyDown(new InputEventArgs(KeyCode.Enter, new KeyModifiers(false, false, false)));
+
+			Assert.AreEqual(0, count);
+
+			c.ProcessKeys("help help");
+			c.ProcessKeyDown(new InputEventArgs(KeyCode.Enter, new KeyModifiers(false, false, false)));
+			Assert.AreEqual(1, count);
+
+			c.ProcessKeys("help unknown_command");
+			c.ProcessKeyDown(new InputEventArgs(KeyCode.Enter, new KeyModifiers(false, false, false)));
+			Assert.AreEqual(1, count);
+		}
+	}
+}
