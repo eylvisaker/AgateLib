dedd12cf85cdba90bc37f999ff5457b8a0cef9f2
Erik Ylvisaker <eylvisaker@gmail.com>
2014-01-20 02:00:50 +0000

    Add InsertionSort extension for list objects.

---
+++ AgateLib/Extensions/Collections/ListSorting.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.Extensions.Collections
+{
+	public static class ListSorting
+	{
+		/// <summary>
+		/// Provides a sort method for IList&lt;T&gt; objects which 
+		/// is stable, unlike the List&lt;T&gt;.Sort() method.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		/// <param name="comparison"></param>
+		public static void InsertionSort<T>(this IList<T> list)
+		{
+			if (list == null) throw new ArgumentNullException("list");
+
+			Comparison<T> comp;
+
+			if (typeof(IComparable<T>).IsAssignableFrom(typeof(T)))
+			{
+				InsertionSort(list, (x, y) => ((IComparable<T>)x).CompareTo(y));
+			}
+			else if (typeof(IComparable).IsAssignableFrom(typeof(T)))
+			{
+				InsertionSort(list, (x, y) => ((IComparable)x).CompareTo(y));
+			}
+		}
+		/// <summary>
+		/// Provides a sort method for IList&lt;T&gt; objects which 
+		/// is stable, unlike the List&lt;T&gt;.Sort() method.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		/// <param name="comparison"></param>
+		public static void InsertionSort<T>(this IList<T> list, Comparison<T> comparison)
+		{
+			if (list == null) throw new ArgumentNullException("list");
+			if (comparison == null) throw new ArgumentNullException("comparison");
+
+			int count = list.Count;
+			for (int i = 1; i < count; i++)
+			{
+				T current = list[i];
+
+				int j = i - 1;
+				for (; j >= 0 && comparison(list[j], current) > 0; j--)
+				{
+					list[j + 1] = list[j];
+				}
+				list[j + 1] = current;
+			}
+		}
+
+	}
+}
---
+++ AgateLib/Extensions/Collections/NonGeneric/NonGenericListSorting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.Extensions.Collections.NonGeneric
+{
+	public static class NonGenericListSorting
+	{
+		/// <summary>
+		/// Provides a sort method for IList&lt;T&gt; objects which 
+		/// is stable, unlike the List&lt;T&gt;.Sort() method.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		/// <param name="comparison"></param>
+		public static void InsertionSort(this IList list)
+		{
+			if (list == null) throw new ArgumentNullException("list");
+
+			int count = list.Count;
+			for (int i = 1; i < count; i++)
+			{
+				object current = list[i];
+
+				int j = i - 1;
+				for (; j >= 0 && Comparer<object>.Default.Compare(list[j], current) > 0; j--)
+				{
+					list[j + 1] = list[j];
+				}
+				list[j + 1] = current;
+			}
+		}
+	}
+}
