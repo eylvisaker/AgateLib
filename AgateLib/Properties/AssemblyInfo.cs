e3253c0edf094b6c4f8df0d345a84f9a5e9b0a08
Erik Ylvisaker <eylvisaker@gmail.com>
2014-07-19 01:25:49 +0000

    Change version number to 1.0.

--- AgateLib/Properties/AssemblyInfo.cs
+++ AgateLib/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.3.5.0")]
-[assembly: AssemblyFileVersion("0.3.5.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
