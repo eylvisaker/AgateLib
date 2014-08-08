81c8448e2d903944df2b59f75cb280b11b2f9a5c
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2014-08-08 01:28:13 +0000

    Refactor object construction during serialization into a platform specific object.

---
+++ trunk/AgateLib/Drivers/IObjectConstructor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgateLib.Drivers
+{
+	public interface IObjectConstructor
+	{
+		object CreateInstance(Type t);
+	}
+}
