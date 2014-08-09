a745fa3473ff49a1a3e2d0c6ca497314180ab80b
Erik Ylvisaker <eylvisaker@gmail.com>
2014-08-09 18:07:20 +0000

    Add Font object.

---
+++ AgateLib/DisplayLib/Font.cs
@@ -0,0 +1,194 @@
+﻿using AgateLib.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgateLib.DisplayLib
+{
+	public class Font
+	{
+		Dictionary<FontSettings, FontSurface> mFontSurfaces = new Dictionary<FontSettings, FontSurface>();
+		FontSettings mSettings;
+		FontState mState = new FontState();
+
+		public Font(string name)
+		{
+			Name = name;
+		}
+
+		public void AddFont(FontSurface fontSurface, int size, FontStyle style)
+		{
+			AddFont(new FontSettings(size, style), fontSurface);
+		}
+		public void AddFont(FontSettings settings, FontSurface fontSurface)
+		{
+			if (Size == 0)
+				mSettings = settings;
+
+			mFontSurfaces[settings] = fontSurface;
+		}
+
+		public string Name { get; set; }
+		public int Size { get { return mSettings.Size; } set { mSettings.Size = value; } }
+		public FontStyle Style { get { return mSettings.Style; } set { mSettings.Style = value; } }
+
+		public int FontHeight { get { return FontSurface.FontHeight; } }
+
+		int MaxSize(FontStyle style)
+		{
+			var keys = mFontSurfaces.Keys.Where(x => x.Style == style);
+			if (keys.Count() > 0)
+				return keys.Max(x => x.Size);
+			else
+				return -1;
+		}
+
+		public FontSurface FontSurface
+		{
+			get
+			{
+				var font = GetClosestFont(mSettings);
+				font.State = mState;
+				return font;
+			}
+		}
+
+		#region --- Finding correctly sized font ---
+
+		public FontSurface GetClosestFont(int size, FontStyle style)
+		{
+			return GetClosestFont(new FontSettings(size, style));
+		}
+		public FontSurface GetClosestFont(FontSettings fontSettings)
+		{
+			var settings = GetClosestFontSettings(fontSettings);
+			var retval = mFontSurfaces[settings];
+
+			var ratio = fontSettings.Size / (double)settings.Size;
+
+			retval.SetScale(ratio, ratio);
+
+			return retval;
+		}
+
+		internal FontSettings GetClosestFontSettings(int size, FontStyle style)
+		{
+			return GetClosestFontSettings(new FontSettings(size, style));
+		}
+		internal FontSettings GetClosestFontSettings(FontSettings settings)
+		{
+			if (mFontSurfaces.ContainsKey(settings))
+				return settings;
+
+			int maxSize = MaxSize(settings.Style);
+
+			// this happens if we have no font surfaces of this style.
+			if (maxSize <= 0)
+			{
+				FontStyle newStyle;
+
+				// OK remove styles until we find an actual font.
+				if (TryRemoveStyle(settings.Style, FontStyle.Strikeout, out newStyle))
+					return GetClosestFontSettings(settings.Size, newStyle);
+				if (TryRemoveStyle(settings.Style, FontStyle.Italic, out newStyle))
+					return GetClosestFontSettings(settings.Size, newStyle);
+				if (TryRemoveStyle(settings.Style, FontStyle.Underline, out newStyle))
+					return GetClosestFontSettings(settings.Size, newStyle);
+				if (TryRemoveStyle(settings.Style, FontStyle.Bold, out newStyle))
+					return GetClosestFontSettings(settings.Size, newStyle);
+				else
+				{
+					Debug.Assert(mFontSurfaces.Count == 0);
+					throw new AgateException("There are no font styles defined.");
+				}
+			}
+
+			if (settings.Size > maxSize)
+				return GetClosestFontSettings(maxSize, settings.Style);
+
+			for (int i = settings.Size; i <= maxSize; i++)
+			{
+				settings.Size = i;
+
+				if (mFontSurfaces.ContainsKey(settings))
+					return settings;
+			}
+
+			throw new AgateException("Could not find a valid font.");
+		}
+
+		#endregion
+
+		private bool TryRemoveStyle(FontStyle value, FontStyle remove, out FontStyle result)
+		{
+			if ((value & remove) == remove)
+			{
+				result = ~(~value | remove);
+				return true;
+			}
+			else
+			{
+				result = 0;
+				return false;
+			}
+		}
+
+
+		public Color Color
+		{
+			get { return mState.Color; }
+			set { mState.Color = value; }
+		}
+		public OriginAlignment DisplayAlignment
+		{
+			get { return mState.DisplayAlignment; }
+			set { mState.DisplayAlignment = value; }
+		}
+
+		public void DrawText(Point dest, string text)
+		{
+			FontSurface.DrawText(dest, text);
+		}
+		public void DrawText(int x, int y, string text)
+		{
+			FontSurface.DrawText(x, y, text);
+		}
+		public void DrawText(int x, int y, string text, params object[] Parameters)
+		{
+			FontSurface.DrawText(x, y, text, Parameters);
+		}
+
+		public Size MeasureString(string text)
+		{
+			return FontSurface.MeasureString(text);
+		}
+	}
+
+	public struct FontSettings : IEquatable<FontSettings>
+	{
+		int mSize;
+		FontStyle mStyle;
+
+		public int Size { get { return mSize; } set { mSize = value; } }
+		public FontStyle Style { get { return mStyle; } set { mStyle = value; } }
+
+		public bool Equals(FontSettings other)
+		{
+			if (Size != other.Size)
+				return false;
+			if (Style != other.Style)
+				return false;
+
+			return true;
+		}
+
+		public FontSettings(int size, FontStyle style)
+		{
+			mSize = size;
+			mStyle = style;
+		}
+	}
+}
---
+++ UnitTests/Display/FontTests.cs
@@ -0,0 +1,70 @@
+﻿using AgateLib;
+using AgateLib.DisplayLib;
+using AgateLib.DisplayLib.ImplementationBase;
+using AgateLib.Geometry;
+using AgateLib.Platform.WindowsForms.ApplicationModels;
+using AgateLib.UnitTests.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgateLib.UnitTests.Display
+{
+	[TestClass]
+	public class FontTests
+	{
+		Font ff;
+
+		[TestInitialize]
+		public void Init()
+		{
+			ff = new Font("times");
+
+			ff.AddFont(new FontSettings(8, FontStyle.None),
+				FontSurface.FromImpl(new FakeFontSurface { Height = 8 }));
+
+			ff.AddFont(new FontSettings(8, FontStyle.Bold),
+				FontSurface.FromImpl(new FakeFontSurface { Height = 8 }));
+
+			ff.AddFont(new FontSettings(10, FontStyle.None),
+				FontSurface.FromImpl(new FakeFontSurface { Height = 10 }));
+
+			ff.AddFont(new FontSettings(10, FontStyle.Bold),
+				FontSurface.FromImpl(new FakeFontSurface { Height = 10 }));
+		}
+		[TestCleanup]
+		public void Terminate()
+		{
+		}
+
+		[TestMethod]
+		public void FFBasicRetrieval()
+		{
+			Assert.AreEqual(new FontSettings(8, FontStyle.None), ff.GetClosestFontSettings(new FontSettings(8, FontStyle.None)));
+			Assert.AreEqual(new FontSettings(10, FontStyle.None), ff.GetClosestFontSettings(new FontSettings(10, FontStyle.None)));
+
+			Assert.AreEqual(new FontSettings(8, FontStyle.None), ff.GetClosestFontSettings(new FontSettings(7, FontStyle.None)));
+			Assert.AreEqual(new FontSettings(10, FontStyle.None), ff.GetClosestFontSettings(new FontSettings(9, FontStyle.None)));
+			Assert.AreEqual(new FontSettings(10, FontStyle.None), ff.GetClosestFontSettings(new FontSettings(11, FontStyle.None)));
+		}
+
+		[TestMethod]
+		public void FFAutoScale()
+		{
+			var font = ff.GetClosestFont(9, FontStyle.None);
+
+			Assert.AreEqual(0.9, font.ScaleHeight, 0.00001);
+		}
+
+		[TestMethod]
+		public void FFRemoveStyleRetrieval()
+		{
+			Assert.AreEqual(new FontSettings(10, FontStyle.None), ff.GetClosestFontSettings(new FontSettings(9, FontStyle.Italic)));
+			Assert.AreEqual(new FontSettings(10, FontStyle.Bold), ff.GetClosestFontSettings(new FontSettings(9, FontStyle.Italic | FontStyle.Bold)));
+
+		}
+	}
+}
