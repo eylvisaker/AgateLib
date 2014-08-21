0eb3c34482511562f475b50d88111e55f84fb4a2
kanato <kanato@e953d628-571c-0410-b8f7-890e41260987>
2014-08-21 06:06:57 +0000

    Remove obsolete code.

--- trunk/AgateLib/UserInterface/Widgets/Window.cs
+++ trunk/AgateLib/UserInterface/Widgets/Window.cs
@@ -27,9 +27,6 @@ namespace AgateLib.UserInterface.Widgets
 {
 	public class Window : Container
 	{
-		string mBackgroundStyle = "paper";
-		string mFrameStyle = "paper";
-
 		public Window()
 		{
 		}
@@ -41,40 +38,5 @@ namespace AgateLib.UserInterface.Widgets
 			: base(contents)
 		{
 		}
-
-		public FontSurface FontSmall { get; set; }
-
-		public string BackgroundStyle
-		{
-			get { return mBackgroundStyle; }
-			set
-			{
-				mBackgroundStyle = value;
-			}
-		}
-		public string FrameStyle
-		{
-			get { return mFrameStyle; }
-			set
-			{
-				mFrameStyle = value;
-			}
-		}
-
-		public override void DrawImpl()
-		{
-			// draw children after drawing frame
-			base.DrawImpl();
-		}
-		public override void Update(double delta_t, ref bool processInput)
-		{
-			base.Update(delta_t, ref processInput);
-		}
-
-		public override void OnInputButtonDown(AgateLib.InputLib.KeyCode button, ref bool handled)
-		{
-			base.OnInputButtonDown(button, ref handled);
-		}
-
 	}
 }
