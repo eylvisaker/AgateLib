95fb3316251cee88ba2b1e58cc277cd22a2cf7cb
Erik Ylvisaker <eylvisaker@gmail.com>
2014-09-05 07:02:24 +0000

    FileSystemProvider returns relative paths in GetAllFiles.

---
+++ AgateLib.Tests/UnitTests/IO/FileSystemProviderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgateLib.Platform.WinForms.IO;
+
+namespace AgateLib.IO
+{
+	[TestClass]
+	public class FileSystemProviderTests
+	{
+		[TestMethod]
+		public void FspRelativePath()
+		{
+			FileSystemProvider fsp = new FileSystemProvider(@"c:\test\path");
+
+			Assert.AreEqual("to/somewhere.txt", 
+				fsp.MakeRelativePath(@"c:\test\path\to\somewhere.txt"));
+
+			Assert.AreEqual("../alternate",
+				fsp.MakeRelativePath(@"c:\test\alternate"));
+		}
+	}
+}
