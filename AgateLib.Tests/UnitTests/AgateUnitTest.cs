bf9d50988e9fd31ddc8f9e73fe3ec56563140fe5
Erik Ylvisaker <eylvisaker@gmail.com>
2015-03-04 17:56:09 -0800

    Stub out some fakes for unit testing.

--- AgateLib.Tests/UnitTests/AgateUnitTest.cs
+++ AgateLib.Tests/UnitTests/AgateUnitTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using AgateLib.Testing.Fakes;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AgateLib.UnitTests
@@ -11,5 +13,13 @@ namespace AgateLib.UnitTests
     [TestClass]
     public class AgateUnitTest
     {
+        public AgateUnitTest()
+        {
+            Factory = new FakeAgateFactory();
+
+            Core.Initialize(Factory);
+        }
+
+        public FakeAgateFactory Factory { get; set; }
     }
 }
---
+++ AgateLib.Tests/UnitTests/Fakes/FakeAudioFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AgateLib.Drivers;
+
+namespace AgateLib.UnitTests.Fakes
+{
+    public class FakeAudioFactory : IAudioFactory
+    {
+        public AudioLib.ImplementationBase.AudioImpl AudioImpl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public AudioLib.ImplementationBase.SoundBufferImpl CreateSoundBuffer(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AudioLib.ImplementationBase.MusicImpl CreateMusic(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AudioLib.ImplementationBase.MusicImpl CreateMusic(System.IO.Stream musicStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AudioLib.ImplementationBase.SoundBufferSessionImpl CreateSoundBufferSession(AudioLib.SoundBufferSession owner, AudioLib.ImplementationBase.SoundBufferImpl buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AudioLib.ImplementationBase.SoundBufferImpl CreateSoundBuffer(System.IO.Stream inStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AudioLib.ImplementationBase.StreamingSoundBufferImpl CreateStreamingSoundBuffer(System.IO.Stream input, AudioLib.SoundFormat format)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
