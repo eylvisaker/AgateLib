237c285586fbddfec5ced4760675d5fa9a2d98ae
Erik Ylvisaker <eylvisaker@gmail.com>
2015-03-04 20:18:53 -0800

    Add missing stubbed files.

---
+++ AgateLib.Tests/UnitTests/Fakes/FakeDisplayFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AgateLib.Drivers;
+
+namespace AgateLib.UnitTests.Fakes
+{
+    public class FakeDisplayFactory : IDisplayFactory
+    {
+        public AgateLib.DisplayLib.ImplementationBase.DisplayImpl DisplayImpl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public AgateLib.DisplayLib.ImplementationBase.DisplayWindowImpl CreateDisplayWindow(AgateLib.DisplayLib.DisplayWindow owner, AgateLib.DisplayLib.CreateWindowParams windowParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AgateLib.DisplayLib.ImplementationBase.SurfaceImpl CreateSurface(IReadFileProvider provider, string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AgateLib.DisplayLib.ImplementationBase.SurfaceImpl CreateSurface(System.IO.Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AgateLib.DisplayLib.ImplementationBase.SurfaceImpl CreateSurface(AgateLib.Geometry.Size surfaceSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AgateLib.DisplayLib.ImplementationBase.SurfaceImpl CreateSurface(AgateLib.DisplayLib.PixelBuffer pixels)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AgateLib.DisplayLib.ImplementationBase.FrameBufferImpl CreateFrameBuffer(AgateLib.Geometry.Size size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeDefaultResourcesAsync(DefaultAssets.DefaultResources res)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AgateLib.Tests/UnitTests/Fakes/FakeFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AgateLib.DisplayLib;
+using AgateLib.Drivers;
+
+namespace AgateLib.UnitTests.Fakes
+{
+    public class FakeFactory : IAgateFactory
+    {
+        FakeDisplayFactory displayFactory = new FakeDisplayFactory();
+        FakeAudioFactory audioFactory = new FakeAudioFactory();
+        FakeInputFactory inputFactory = new FakeInputFactory();
+        FakePlatformFactory platformFactory = new FakePlatformFactory();
+
+        public FakeDisplayFactory DisplayFactory { get { return displayFactory; } }
+        public FakeAudioFactory AudioFactory { get { return audioFactory; } }
+        public FakeInputFactory InputFactory { get { return inputFactory; } }
+        public FakePlatformFactory PlatformFactory { get { return platformFactory; } }
+
+        IDisplayFactory IAgateFactory.DisplayFactory { get { return displayFactory; } }
+        IAudioFactory IAgateFactory.AudioFactory { get { return audioFactory; } }
+        IInputFactory IAgateFactory.InputFactory { get { return inputFactory; } }
+        IPlatformFactory IAgateFactory.PlatformFactory { get { return platformFactory; } }
+
+
+    }
+}
