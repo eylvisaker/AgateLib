95c62efb5069f55a086fc90758ea7750a96c1c59
Erik Ylvisaker <eylvisaker@gmail.com>
2015-09-19 22:33:31 -0700

    Fix test scripts.

--- Coverage.bat
+++ Coverage.bat
@@ -1,2 +1,2 @@
-OpenCover.Console.exe "-target:C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" -targetargs:/testcontainer:AgateLib.Tests\UnitTests\bin\Debug\AgateLib.UnitTests.dll -targetargs:/noisolation -excludebyfile:*\*.Designer.cs -output:Coverage.xml 
+OpenCover.Console.exe "-target:C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" -targetargs:/testcontainer:AgateLib.Tests\UnitTests\bin\Debug\AgateLib.UnitTests.dll -targetargs:/noisolation -excludebyfile:*\*.Designer.cs -output:Coverage.xml -register:user
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
--- UnitTest.bat
+++ UnitTest.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" -targetargs:/testcontainer:AgateLib.Tests\UnitTests\bin\Debug\AgateLib.UnitTests.dll
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" /testcontainer:AgateLib.Tests\UnitTests\bin\Debug\AgateLib.UnitTests.dll
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
