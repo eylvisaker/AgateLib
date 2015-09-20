0d877e172bf51db366ab1865f33220e2f785239d
Erik Ylvisaker <eylvisaker@gmail.com>
2015-09-20 10:48:27 -0700

    Add nuspec file for AgateLib.dll.

--- AgateLib.AgateSDL/Properties/AssemblyInfo.cs
+++ AgateLib.AgateSDL/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AgateLib.SDL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides AgateLib bindings to SDL for input and audio.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AgateLib")]
 [assembly: AssemblyProduct("AgateLib.SDL")]
