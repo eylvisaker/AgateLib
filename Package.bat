8c29daadb3c6f8fa74930ad252860aa51ae8d837
Erik Ylvisaker <eylvisaker@gmail.com>
2015-09-20 18:53:02 -0700

    Add script to create nuget package.

---
+++ Package.bat
@@ -0,0 +1,7 @@
+
+nuget pack AgateLib/AgateLib.csproj
+nuget pack AgateLib.AgateSDL/AgateLib.SDL.csproj
+nuget pack AgateLib.OpenGL/AgateLib.OpenGL.csproj
+nuget pack AgateLib.Platform.WinForms/AgateLib.Platform.WinForms.csproj
+nuget pack AgateLib.Platform.Test/AgateLib.Platform.Test.csproj
+nuget pack AgateLib.Platform.IntegrationTest/AgateLib.Platform.IntegrationTest.csproj
