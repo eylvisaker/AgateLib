fbac45dbaf88b8d574b2a2b56c1ccbcfdd6514db
Erik Ylvisaker <eylvisaker@gmail.com>
2015-10-27 22:06:26 -0700

    Output debug message in nuget install script.

--- AgateLib.AgateSDL/tools/Install.ps1
+++ AgateLib.AgateSDL/tools/Install.ps1
@@ -1,4 +1,9 @@
-﻿param($installPath, $toolsPath, $package, $project)
+﻿param(
+	$installPath, 
+	$toolsPath, 
+	$package, 
+	$project
+)
 
 $filenames = "libFLAC-8.dll", "libmikmod-2.dll", "libmodplug-1.dll", "libogg-0.dll", "libvorbis-0.dll", "libvorbisfile-3.dll", "SDL2.dll", "SDL2_mixer.dll", "smpeg2.dll", "LICENSE.FLAC.txt", "LICENSE.mikmod.txt", "LICENSE.modplug.txt", "LICENSE.smpeg.txt", "README-SDL.txt", "README-SDL_mixer.txt"
 $libfolders = "lib32", "lib64"
@@ -16,6 +21,8 @@ ForEach ($libfolder in $libfolders)
 	ForEach ($filename in $filenames) 
 	{
 		$path = ("{0}\{1}\{2}" -f $toolsPath,$libfolder,$filename);
+		"$path exists: $(test-path $path)"
+
 		$file = $folderItem.ProjectItems.AddFromFile($filename);
 		
 		if ($file -ne $null)
