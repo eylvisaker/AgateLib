7762d8ca79c69f18922727ea2a90ad55dc8b35e0
Erik Ylvisaker <eylvisaker@gmail.com>
2016-01-03 09:11:57 -0800

    Implement more fake surface constructors.

--- AgateLib.Platform.Test/FakeDisplayFactory.cs
+++ AgateLib.Platform.Test/FakeDisplayFactory.cs
@@ -7,64 +7,65 @@ using AgateLib.DefaultAssets;
 using AgateLib.DisplayLib.BitmapFont;
 using AgateLib.DisplayLib.ImplementationBase;
 using AgateLib.Drivers;
+using AgateLib.Geometry;
 
 namespace AgateLib.Platform.Test
 {
-    public class FakeDisplayFactory : IDisplayFactory
-    {
-        public FakeDisplayFactory()
-        {
-            DisplayImpl = new FakeDisplayDriver();
-        }
+	public class FakeDisplayFactory : IDisplayFactory
+	{
+		public FakeDisplayFactory()
+		{
+			DisplayImpl = new FakeDisplayDriver();
+		}
 
-        public DisplayImpl DisplayImpl { get; private set; }
+		public DisplayImpl DisplayImpl { get; private set; }
 
-        public DisplayWindowImpl CreateDisplayWindow(AgateLib.DisplayLib.DisplayWindow owner, AgateLib.DisplayLib.CreateWindowParams windowParams)
-        {
-            return new FakeDisplayWindow(owner, windowParams);
-        }
+		public DisplayWindowImpl CreateDisplayWindow(AgateLib.DisplayLib.DisplayWindow owner, AgateLib.DisplayLib.CreateWindowParams windowParams)
+		{
+			return new FakeDisplayWindow(owner, windowParams);
+		}
 
-        public SurfaceImpl CreateSurface(IReadFileProvider fileProvider, string fileName)
-        {
-            return new FakeSurface();
-        }
+		public SurfaceImpl CreateSurface(IReadFileProvider fileProvider, string fileName)
+		{
+			return new FakeSurface(new Size(10, 10));
+		}
 
-        public SurfaceImpl CreateSurface(System.IO.Stream fileStream)
-        {
-            throw new NotImplementedException();
-        }
+		public SurfaceImpl CreateSurface(System.IO.Stream fileStream)
+		{
+			throw new NotImplementedException();
+		}
 
-        public SurfaceImpl CreateSurface(AgateLib.Geometry.Size surfaceSize)
-        {
-            throw new NotImplementedException();
-        }
+		public SurfaceImpl CreateSurface(Size surfaceSize)
+		{
+			return new FakeSurface(surfaceSize);
+		}
 
-        public FontSurfaceImpl CreateFont(string fontFamily, float sizeInPoints, AgateLib.DisplayLib.FontStyles style)
-        {
-            throw new NotImplementedException();
-        }
+		public FontSurfaceImpl CreateFont(string fontFamily, float sizeInPoints, AgateLib.DisplayLib.FontStyles style)
+		{
+			throw new NotImplementedException();
+		}
 
-        public FontSurfaceImpl CreateFont(BitmapFontOptions bitmapOptions)
-        {
-            throw new NotImplementedException();
-        }
+		public FontSurfaceImpl CreateFont(BitmapFontOptions bitmapOptions)
+		{
+			throw new NotImplementedException();
+		}
 
-        public FrameBufferImpl CreateFrameBuffer(AgateLib.Geometry.Size size)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public SurfaceImpl CreateSurface(DisplayLib.PixelBuffer pixels)
-        {
-            throw new NotImplementedException();
-        }
+		public FrameBufferImpl CreateFrameBuffer(AgateLib.Geometry.Size size)
+		{
+			throw new NotImplementedException();
+		}
 
 
-        public Task InitializeDefaultResourcesAsync(DefaultResources res)
-        {
-            return Task.Run(() => { });
-        }
-    }
+		public SurfaceImpl CreateSurface(DisplayLib.PixelBuffer pixels)
+		{
+			return new FakeSurface(pixels.Size);
+		}
+
+
+		public Task InitializeDefaultResourcesAsync(DefaultResources res)
+		{
+			return Task.Run(() => { });
+		}
+	}
 
 }
--- AgateLib.Platform.Test/FakeSurface.cs
+++ AgateLib.Platform.Test/FakeSurface.cs
@@ -2,57 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using AgateLib.DisplayLib.ImplementationBase;
+using AgateLib.Geometry;
 
 namespace AgateLib.Platform.Test
 {
-    public class FakeSurface : SurfaceImpl
-    {
-        protected override void Dispose(bool disposing)
-        {
-        }
+	public class FakeSurface : SurfaceImpl
+	{
+		private readonly Size size;
 
-        public override void Draw(DisplayLib.SurfaceState state)
-        {
-            throw new NotImplementedException();
-        }
+		public FakeSurface(Size size)
+		{
+			this.size = size;
+		}
 
-        public override void SaveTo(string filename, DisplayLib.ImageFileFormat format)
-        {
-            throw new NotImplementedException();
-        }
+		protected override void Dispose(bool disposing)
+		{
+		}
 
-        public override SurfaceImpl CarveSubSurface(Geometry.Rectangle srcRect)
-        {
-            throw new NotImplementedException();
-        }
+		public override void Draw(DisplayLib.SurfaceState state)
+		{
+		}
 
-        public override void SetSourceSurface(SurfaceImpl surf, Geometry.Rectangle srcRect)
-        {
-            throw new NotImplementedException();
-        }
+		public override void SaveTo(string filename, DisplayLib.ImageFileFormat format)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override DisplayLib.PixelBuffer ReadPixels(DisplayLib.PixelFormat format, Geometry.Rectangle rect)
-        {
-            throw new NotImplementedException();
-        }
+		public override SurfaceImpl CarveSubSurface(Rectangle srcRect)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override void WritePixels(DisplayLib.PixelBuffer buffer)
-        {
-            throw new NotImplementedException();
-        }
+		public override void SetSourceSurface(SurfaceImpl surf, Rectangle srcRect)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override Geometry.Size SurfaceSize
-        {
-            get { throw new NotImplementedException(); }
-        }
+		public override DisplayLib.PixelBuffer ReadPixels(DisplayLib.PixelFormat format, Rectangle rect)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override bool IsLoaded
-        {
-            get { throw new NotImplementedException(); }
-        }
+		public override void WritePixels(DisplayLib.PixelBuffer buffer)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override event EventHandler LoadComplete;
-    }
+		public override Size SurfaceSize
+		{
+			get { return size; }
+		}
+
+		public override bool IsLoaded
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override event EventHandler LoadComplete;
+	}
 }
