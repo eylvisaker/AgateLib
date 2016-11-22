d60c56dfba0bc76b6f7c8a135567d629392b8b1b
Erik Ylvisaker <eylvisaker@gmail.com>
2016-11-21 23:10:27 -0800

    Add tests for borders.

--- AgateLib.Tests/UnitTests/UserInterface/Venus/AdapterTests.cs
+++ AgateLib.Tests/UnitTests/UserInterface/Venus/AdapterTests.cs
@@ -7,6 +7,7 @@ using AgateLib.Geometry;
 using AgateLib.Resources;
 using AgateLib.Resources.Managers;
 using AgateLib.UnitTests.Resources;
+using AgateLib.UserInterface.DataModel;
 using AgateLib.UserInterface.Rendering;
 using AgateLib.UserInterface.Venus;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -46,6 +47,35 @@ namespace AgateLib.UnitTests.UserInterface.Venus
 			Assert.AreEqual(BackgroundRepeat.None, style.Background.Repeat);
 			Assert.AreEqual(BackgroundClip.Content, style.Background.Clip);
 			Assert.AreEqual(new Point(4, 3), style.Background.Position);
+		}
+		
+		[TestMethod]
+		public void AdapterBorderProperties()
+		{
+			const string image = "abc123.png";
+			LayoutBox borderSlice = new LayoutBox { Left = 2, Top = 3, Right = 4, Bottom = 5 };
+
+			var windowTheme = uiManager.Adapter.ThemeData.First().Value["window"];
+			windowTheme.Border = new WidgetBorderModel();
+			windowTheme.Border.Image = image;
+			windowTheme.Border.Slice = borderSlice;
+
+			var facet = new ResourceManagerInitializer.TestFacet();
+			uiManager.InitializeFacet(facet);
+
+			var style = adapter.StyleOf(facet.WindowA);
+			var border = style.Border;
+
+			Assert.AreEqual(image, style.Border.Image);
+			Assert.AreEqual(borderSlice, style.Border.ImageSlice);
+			Assert.AreEqual("ui_back_1.png", style.Border.Left.Width);
+			Assert.AreEqual("ui_back_1.png", style.Border.Left.Color);
+			Assert.AreEqual("ui_back_1.png", style.Border.Top.Width);
+			Assert.AreEqual("ui_back_1.png", style.Border.Top.Color);
+			Assert.AreEqual("ui_back_1.png", style.Border.Right.Width);
+			Assert.AreEqual("ui_back_1.png", style.Border.Right.Color);
+			Assert.AreEqual("ui_back_1.png", style.Border.Bottom.Width);
+			Assert.AreEqual("ui_back_1.png", style.Border.Bottom.Color);
 		}
 	}
 }
