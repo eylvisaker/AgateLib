a70f7fddfb6a65057d2375d1039635ac44115b88
Erik Ylvisaker <eylvisaker@gmail.com>
2016-12-29 20:49:45 -0800

    Move nuget packages to subdirectory.

--- Package.bat
+++ Package.bat
@@ -5,3 +5,6 @@ nuget pack AgateLib.OpenGL/AgateLib.OpenGL.csproj
 nuget pack AgateLib.Platform.WinForms/AgateLib.Platform.WinForms.csproj
 nuget pack AgateLib.Platform.Test/AgateLib.Platform.Test.csproj
 nuget pack AgateLib.Platform.IntegrationTest/AgateLib.Platform.IntegrationTest.csproj
+
+mkdir packages
+move *.nupkg packages
