c8bde47a8aa6e179d6451eb99c6345d080aaa394
Erik Ylvisaker <eylvisaker@gmail.com>
2017-02-07 23:15:06 -0800

    Move time tracker code into MasterClock.

--- AgateLib.Tests.UnitTests/Platform/TimingTests.cs
+++ AgateLib.Tests.UnitTests/Platform/TimingTests.cs
@@ -21,21 +21,21 @@ namespace AgateLib.UnitTests.Platform
 		public void AdvanceTest()
 		{
 			var watch = new FakeStopwatch();
-			var clock = new MasterClock(watch);
-			var tracker = new GameClock(clock);
+			var masterClock = new MasterClock(watch);
+			var gameClock = new GameClock(masterClock);
 
 			watch.CurrentTime = TimeSpan.FromSeconds(1);
-			clock.Advance();
+			masterClock.Advance();
 
 			watch.CurrentTime = TimeSpan.FromSeconds(2);
-			clock.Advance();
+			masterClock.Advance();
 
-			Assert.AreEqual(1, tracker.DeltaTime.TotalSeconds, 0.001);
+			Assert.AreEqual(1, gameClock.DeltaTime.TotalSeconds, 0.001);
 
 			watch.CurrentTime = TimeSpan.FromSeconds(0.5);
-			clock.Advance();
+			masterClock.Advance();
 
-			Assert.AreEqual(0.5, tracker.DeltaTime.TotalSeconds, 0.001);
+			Assert.AreEqual(0.5, gameClock.DeltaTime.TotalSeconds, 0.001);
 		}
 	}
 }
--- AgateLib/Platform/MasterClock.cs
+++ AgateLib/Platform/MasterClock.cs
@@ -9,16 +9,82 @@ namespace AgateLib.Platform
 {
 	public class MasterClock : IStopClock
 	{
-		private IClock root;
+		private readonly IClock root;
+
+		private const double MinDeltaTime = 0.0000001;
+
+		private double currentTimeMs;
+		private double lastTime;
+		private bool ranOnce;
+
+		private double fpsStart;
+		private int frames;
+		private double fps;
 
 		public MasterClock(IClock rootClock)
 		{
 			this.root = rootClock;
 		}
 
+		/// <summary>
+		/// Gets or sets the number of times per second
+		/// that the FPS should be updated. Larger values
+		/// will update slower but the results will be more
+		/// consistent. The default is 5 updates per second.
+		/// </summary>
+		public double FpsUpdateFrequency { get; set; } = 5;
+
+		public TimeSpan DeltaTime { get; private set; }
+
+		public TimeSpan CurrentTime => TimeSpan.FromMilliseconds(currentTimeMs);
+
+		public double FramesPerSecond => fps;
+
+		/// <summary>
+		/// Advances the time tracker and computes the amount of time passed since 
+		/// </summary>
 		public void Advance()
 		{
-			throw new NotImplementedException();
+			double now = root.CurrentTime.TotalMilliseconds;
+			double timePassedMs = 0;
+
+			frames++;
+
+			if (ranOnce)
+			{
+				timePassedMs = now - lastTime;
+				lastTime = now;
+
+				currentTimeMs += timePassedMs;
+
+				if ((now - fpsStart) > 1000 / FpsUpdateFrequency)
+				{
+					double time = (now - fpsStart) * 0.001;
+
+					// average current framerate with that of the last update
+					fps = (frames / time) * 0.8 + fps * 0.2;
+
+					fpsStart = now;
+					frames = 0;
+				}
+
+				// hack to make sure delta time is never zero.
+				if (timePassedMs < MinDeltaTime)
+				{
+					timePassedMs = MinDeltaTime;
+				}
+			}
+			else
+			{
+				lastTime = now;
+
+				fpsStart = now;
+				frames = 0;
+
+				ranOnce = true;
+			}
+
+			DeltaTime = TimeSpan.FromMilliseconds(timePassedMs);
 		}
 	}
 }
