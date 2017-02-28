8c2baff892be336a78a1a11af865c90676130c65
Erik Ylvisaker <eylvisaker@gmail.com>
2017-02-28 07:51:34 -0800

    Add nuspec to AgateLib.Physics and correct namespace.

--- AgateLib.Physics/VectorExtensions.cs
+++ AgateLib.Physics/VectorExtensions.cs
@@ -9,13 +9,6 @@ namespace AgateLib.Physics
 {
 	public static class VectorExtensions
 	{
-		public static Vector2 Rotate(this Vector2 parent, double angle)
-		{
-			return new Vector2(
-				Math.Cos(angle) * parent.X + Math.Sin(angle) * parent.Y,
-				-Math.Sin(angle) * parent.X + Math.Cos(angle) * parent.Y);
-		}
-
 		public static Vector2 RotationDerivative(this Vector2 parent, double angle)
 		{
 			return new Vector2(
