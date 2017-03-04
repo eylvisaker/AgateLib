f2bbf87ff011da2a587dc4fc781b8f8ff5fbeb70
Erik Ylvisaker <eylvisaker@gmail.com>
2017-03-03 18:45:39 -0800

    Add annotations for AgateLib.Physics.

--- AgateLib.Physics/Properties/AssemblyInfo.cs
+++ AgateLib.Physics/Properties/AssemblyInfo.cs
@@ -6,15 +6,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RigidBodyDynamics")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AgateLib Physics Engine")]
+[assembly: AssemblyDescription("Physics engine for use with AgateLib")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RigidBodyDynamics")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("AgateLib")]
+[assembly: AssemblyProduct("AgateLib")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
