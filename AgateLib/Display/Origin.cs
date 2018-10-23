214fb54c730ec6aa958d2e04b3216763634411b0
Erik Ylvisaker <eylvisaker@gmail.com>
2018-10-23 08:01:59 -0700

    Origin static class methods are now available as extension methods.

--- AgateLib/Display/Origin.cs
+++ AgateLib/Display/Origin.cs
@@ -20,142 +20,160 @@
 //    SOFTWARE.
 //
 
-using System;
-using System.Collections.Generic;
 using AgateLib.Mathematics.Geometry;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace AgateLib.Display
 {
-	/// <summary>
-	/// Static class which performs necessary calculates based on OriginAlignment
-	/// values.
-	/// </summary>
-	public static class Origin
-	{
-		/// <summary>
-		/// Returns a point which should be subtracted from the interpreted
-		/// point to get the top-left position.
-		/// </summary>
-		/// <param name="origin"></param>
-		/// <param name="size"></param>
-		/// <returns></returns>
-		public static Point Calc(OriginAlignment origin, Size size)
-		{
-			Point result = new Point();
-			int tb = ((int)origin) & 0xf0;
-			int lr = ((int)origin) & 0x0f;
-			int width = Math.Abs(size.Width);
-			int height = Math.Abs(size.Height);
+    /// <summary>
+    /// Static class which performs necessary calculates based on OriginAlignment
+    /// values.
+    /// </summary>
+    public static class Origin
+    {
+        /// <summary>
+        /// Return the point on the rectangle that corresponds to the specified alignemtn.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static Point OnRect(this OriginAlignment origin, Rectangle rect)
+        {
+            var calcPoint = Calc(origin, rect.Size);
 
-			if (lr == 0x01)
-				result.X = 0;
-			else if (lr == 0x02)
-				result.X = width / 2;
-			else if (lr == 0x03)
-			{
-				result.X = width;
+            return new Point(calcPoint.X + rect.X,
+                             calcPoint.Y + rect.Y);
+        }
 
-			}
+        /// <summary>
+        /// Returns a point which should be subtracted from the interpreted
+        /// point to get the top-left position.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Point Calc(this OriginAlignment origin, Size size)
+        {
+            Point result = new Point();
+            int tb = ((int)origin) & 0xf0;
+            int lr = ((int)origin) & 0x0f;
+            int width = Math.Abs(size.Width);
+            int height = Math.Abs(size.Height);
 
-			if (tb == 0x10)
-				result.Y = 0;
-			else if (tb == 0x20)
-				result.Y = height / 2;
-			else if (tb == 0x30)
-			{
-				result.Y = height;
-			}
+            if (lr == 0x01)
+                result.X = 0;
+            else if (lr == 0x02)
+                result.X = width / 2;
+            else if (lr == 0x03)
+            {
+                result.X = width;
 
-			return result;
-		}
-		/// <summary>
-		/// Returns a point which should be subtracted from the interpreted
-		/// point to get the top-left position.
-		/// </summary>
-		/// <param name="origin"></param>
-		/// <param name="size"></param>
-		/// <returns></returns>
-		public static Vector2 CalcF(OriginAlignment origin, Size size)
-		{
-			Point result = Calc(origin, size);
+            }
 
-			return new Vector2(result.X, result.Y);
-		}
-		/// <summary>
-		/// Returns a point which should be subtracted from the interpreted
-		/// point to get the top-left position.
-		/// </summary>
-		/// <param name="origin"></param>
-		/// <param name="size"></param>
-		/// <returns></returns>
-		public static Vector2 CalcF(OriginAlignment origin, SizeF size)
-		{
-			Vector2 result = new Vector2();
-			int tb = ((int)origin) & 0xf0;
-			int lr = ((int)origin) & 0x0f;
+            if (tb == 0x10)
+                result.Y = 0;
+            else if (tb == 0x20)
+                result.Y = height / 2;
+            else if (tb == 0x30)
+            {
+                result.Y = height;
+            }
 
-			if (lr == 0x01)
-				result.X = 0;
-			else if (lr == 0x02)
-				result.X = size.Width / 2;
-			else if (lr == 0x03)
-				result.X = size.Width;
+            return result;
+        }
 
-			if (tb == 0x10)
-				result.Y = 0;
-			else if (tb == 0x20)
-				result.Y = size.Height / 2;
-			else if (tb == 0x30)
-				result.Y = size.Height;
+        /// <summary>
+        /// Returns a point which should be subtracted from the interpreted
+        /// point to get the top-left position.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Vector2 CalcF(this OriginAlignment origin, Size size)
+        {
+            Point result = Calc(origin, size);
 
-			return result;
-		}
-		/// <summary>
-		/// Returns a point which should be subtracted from the interpreted
-		/// point to get the top-left position.
-		/// </summary>
-		/// <param name="origin"></param>
-		/// <param name="size"></param>
-		/// <param name="x"></param>
-		/// <param name="y"></param>
-		public static void Calc(OriginAlignment origin, Size size, out int x, out int y)
-		{
-			Point ret = Calc(origin, size);
+            return new Vector2(result.X, result.Y);
+        }
 
-			x = ret.X;
-			y = ret.Y;
+        /// <summary>
+        /// Returns a point which should be subtracted from the interpreted
+        /// point to get the top-left position.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Vector2 CalcF(this OriginAlignment origin, SizeF size)
+        {
+            Vector2 result = new Vector2();
+            int tb = ((int)origin) & 0xf0;
+            int lr = ((int)origin) & 0x0f;
 
-		}
-		/// <summary>
-		/// Modifies the rectangle by taking its Location and converting it
-		/// using Calc so that the rectangle outlines the actual destination.
-		/// </summary>
-		/// <param name="origin"></param>
-		/// <param name="rect"></param>
-		/// <returns></returns>
-		public static Rectangle CalcRect(OriginAlignment origin, Rectangle rect)
-		{
-			return CalcRect(origin, rect, rect.Size);
-		}
-		/// <summary>
-		/// Modifies the rectangle by taking its Location and converting it
-		/// using Calc so that the rectangle outlines the actual destination.
-		/// </summary>
-		/// <param name="origin"></param>
-		/// <param name="rect"></param>
-		/// <param name="effectiveSize"></param>
-		/// <returns></returns>
-		public static Rectangle CalcRect(OriginAlignment origin, Rectangle rect, Size effectiveSize)
-		{
-			Point pt = Origin.Calc(origin, effectiveSize);
-			Rectangle result = new Rectangle(rect.X, rect.Y, rect.Width, rect.Height);
+            if (lr == 0x01)
+                result.X = 0;
+            else if (lr == 0x02)
+                result.X = size.Width / 2;
+            else if (lr == 0x03)
+                result.X = size.Width;
 
-			result.X -= pt.X;
-			result.Y -= pt.Y;
+            if (tb == 0x10)
+                result.Y = 0;
+            else if (tb == 0x20)
+                result.Y = size.Height / 2;
+            else if (tb == 0x30)
+                result.Y = size.Height;
 
-			return result;
-		}
-	};
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a point which should be subtracted from the interpreted
+        /// point to get the top-left position.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="size"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public static void Calc(this OriginAlignment origin, Size size, out int x, out int y)
+        {
+            Point ret = Calc(origin, size);
+
+            x = ret.X;
+            y = ret.Y;
+
+        }
+
+        /// <summary>
+        /// Modifies the rectangle by taking its Location and converting it
+        /// using Calc so that the rectangle outlines the actual destination.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static Rectangle CalcRect(this OriginAlignment origin, Rectangle rect)
+        {
+            return CalcRect(origin, rect, rect.Size);
+        }
+
+        /// <summary>
+        /// Modifies the rectangle by taking its Location and converting it
+        /// using Calc so that the rectangle outlines the actual destination.
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="rect"></param>
+        /// <param name="effectiveSize"></param>
+        /// <returns></returns>
+        public static Rectangle CalcRect(this OriginAlignment origin, Rectangle rect, Size effectiveSize)
+        {
+            Point pt = Origin.Calc(origin, effectiveSize);
+            Rectangle result = new Rectangle(rect.X, rect.Y, rect.Width, rect.Height);
+
+            result.X -= pt.X;
+            result.Y -= pt.Y;
+
+            return result;
+        }
+    };
 
 }
