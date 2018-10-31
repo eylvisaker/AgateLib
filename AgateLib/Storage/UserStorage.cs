7c34a6d8111f8c8ace72fd31d50b812f2a363e7b
Erik Ylvisaker <eylvisaker@gmail.com>
2018-10-31 06:03:09 -0700

    Add UserStorage class.

---
+++ AgateLib/Storage/UserStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Text;
+
+namespace AgateLib.Storage
+{
+    [Singleton]
+    public class UserStorage
+    {
+        private IsolatedStorageFile iso;
+
+        public UserStorage()
+        {
+            //iso = IsolatedStorageFile.GetUserStoreForApplication();
+            iso = IsolatedStorageFile.GetUserStoreForDomain();
+        }
+
+        public Stream OpenFile(string path, FileMode fileMode)
+        {
+            return iso.OpenFile(path, fileMode);
+        }
+
+        public Stream OpenFile(string path, FileMode fileMode, FileAccess fileAccess)
+        {
+            return iso.OpenFile(path, fileMode, fileAccess);
+        }
+
+        public Stream OpenFile(string path, FileMode fileMode, FileAccess fileAccess, FileShare fileShare)
+        {
+            return iso.OpenFile(path, fileMode, fileAccess, fileShare);
+        }
+    }
+}
