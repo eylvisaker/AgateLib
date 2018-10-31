fcdb53627e9955f1351c4c8c9233a308200e0c51
Erik Ylvisaker <eylvisaker@gmail.com>
2018-10-31 07:21:10 -0700

    Add console manager and scene to make it easier to use.

---
+++ AgateLib/Diagnostics/AgateConsoleManager.cs
@@ -0,0 +1,74 @@
+﻿using AgateLib;
+using AgateLib.Diagnostics;
+using AgateLib.Scenes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace AgateLib.Diagnostics
+{
+    public interface IAgateConsoleManager
+    {
+        event Action Quit;
+
+        void Update(GameTime gameTime);
+
+        void AddVocabulary(params IVocabulary[] vocab);
+    }
+
+    [Singleton]
+    public class AgateConsoleManager : IAgateConsoleManager
+    {
+        private readonly ISceneStack sceneStack;
+        private readonly AgateConsole console;
+        private readonly AgateConsoleScene consoleScene;
+
+        public AgateConsoleManager(ISceneStack sceneStack, AgateConsole console, AgateConsoleScene consoleScene)
+        {
+            this.sceneStack = sceneStack;
+            this.console = console;
+            this.consoleScene = consoleScene;
+        }
+
+        /// <summary>
+        /// Action to carry out when the user types quit.
+        /// </summary>
+        public event Action Quit
+        {
+            add => console.State.Quit += value;
+            remove => console.State.Quit -= value;
+        }
+
+        /// <summary>
+        /// Gets or sets the keyboard key that can be used to open the console.
+        /// </summary>
+        public Keys ActivationKey { get; set; } = Keys.OemTilde;
+
+        /// <summary>
+        /// Adds a vocabulary object to the console.
+        /// </summary>
+        /// <param name="vocabs"></param>
+        public void AddVocabulary(params IVocabulary[] vocabs)
+        {
+            foreach (IVocabulary vocab in vocabs)
+                console.AddCommands(vocab);
+        }
+
+        /// <summary>
+        /// Updates the console.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+            if (sceneStack.Contains(consoleScene))
+                return;
+
+            var keys = Keyboard.GetState();
+
+            if (keys.IsKeyDown(ActivationKey))
+            {
+                sceneStack.Add(consoleScene);
+            }
+        }
+    }
+}
---
+++ AgateLib/Diagnostics/AgateConsoleScene.cs
@@ -0,0 +1,52 @@
+﻿using AgateLib;
+using AgateLib.Diagnostics;
+using AgateLib.Scenes;
+using AgateLib.UserInterface;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgateLib.Diagnostics
+{
+    [Singleton]
+    public class AgateConsoleScene : Scene
+    {
+        private readonly AgateConsole console;
+        private readonly IFontProvider fontProvider;
+
+        public AgateConsoleScene(AgateConsole console, IFontProvider fontProvider)
+        {
+            this.console = console;
+            this.fontProvider = fontProvider;
+
+            UpdateBelow = false;
+            DrawBelow = true;
+
+            console.ConsoleClosed += () => IsFinished = true;
+        }
+
+        protected override void OnSceneStart()
+        {
+            base.OnSceneStart();
+
+            IsFinished = false;
+
+            console.Open();
+        }
+
+        protected override void OnUpdate(GameTime time)
+        {
+            base.OnUpdate(time);
+
+            console.Update(time);
+        }
+
+        protected override void DrawScene(GameTime time)
+        {
+            base.DrawScene(time);
+
+            console.Draw(time);
+        }
+    }
+}
