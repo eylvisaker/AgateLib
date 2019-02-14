4edd660349e310192301a1a287d1df808cae301e
Erik Ylvisaker <eylvisaker@gmail.com>
2019-02-14 08:15:18 -0800

    Create test which demos problem of widgets in lower workspaces not getting updated.

---
+++ AgateLib.Tester/UserInterface/MultipleWorkspaces/MultiWorkspaceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgateLib.UserInterface;
+
+namespace AgateLib.Tests.UserInterface.MultipleWorkspaces
+{
+    public class MultiWorkspaceTest : UITest
+    {
+        public override string Name => "Multiple Workspaces";
+
+        protected override IRenderable CreateUIRoot()
+            => new MultiWorkspaceApp(new MultiWorkspaceAppProps());
+    }
+}
---
+++ AgateLib.Tester/UserInterface/MultipleWorkspaces/StatusWindow.cs
@@ -0,0 +1,30 @@
+﻿using AgateLib.UserInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgateLib.Tests.UserInterface.MultipleWorkspaces
+{
+    public class StatusWindow : Widget<StatusWindowProps>
+    {
+        public StatusWindow(StatusWindowProps props) : base(props)
+        {
+        }
+
+        public override IRenderable Render() =>
+            new Window(new WindowProps
+            {
+                Children =
+                {
+                    new Label(new LabelProps { Text = Props.Text })
+                }
+            });
+    }
+
+    public class StatusWindowProps : WidgetProps
+    {
+        public string Text { get; set; }
+    }
+}
