e8f25a8852a3eba3de31143ef59db80446055038
Erik Ylvisaker <eylvisaker@gmail.com>
2019-06-02 18:53:36 -0700

    Move away from using isolated storage for desktops, because it changes for different versions.

--- AgateLib/Storage/UserStorage.cs
+++ AgateLib/Storage/UserStorage.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.IsolatedStorage;
-using System.Text;
 
 namespace AgateLib.Storage
 {
@@ -11,6 +9,15 @@ namespace AgateLib.Storage
     /// </summary>
     public interface IUserStorage
     {
+        /// <summary>
+        /// For desktop platforms, sets the folder inside the root app data folder for the storage
+        /// for this user.
+        /// </summary>
+        /// <param name="folderName">Name of the folder. Recommended is to use
+        /// company_name/product_name, eg. "VermilionTower/ThornbridgeSaga"</param>
+        /// <returns></returns>
+        void SetApplicationDataFolder(string folderName);
+
         bool FileExists(string path);
         Stream OpenFile(string path, FileMode fileMode);
         Stream OpenFile(string path, FileMode fileMode, FileAccess fileAccess);
@@ -23,15 +30,103 @@ namespace AgateLib.Storage
     [Singleton]
     public class UserStorage : IUserStorage
     {
+#if DESKTOP
+        private bool appDataFolderSet, noRootWarning;
+
+        private string appDataFolder;
+        private string rootFolder;
+
+        public UserStorage()
+        {
+            appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            rootFolder = Path.Combine(appDataFolder, "UnknownApplication");
+        }
+
+        public void SetApplicationDataFolder(string folderName)
+        {
+            if (string.IsNullOrWhiteSpace(folderName))
+                throw new ArgumentException(nameof(folderName));
+
+            rootFolder = Path.Combine(appDataFolder, folderName);
+
+            appDataFolderSet = true;
+            noRootWarning = false;
+
+            Log.Info($"User storage set to {rootFolder}.");
+        }
+
+        public bool FileExists(string path)
+        {
+            WarnIfNoRoot();
+
+            return File.Exists(Path.Combine(rootFolder, path));
+        }
+
+        public Stream OpenFile(string path, FileMode fileMode)
+        {
+            WarnIfNoRoot();
+
+            CreateDirectoryIfApplicable(path, fileMode);
+
+            return File.Open(path, fileMode);
+        }
+
+        public Stream OpenFile(string path, FileMode fileMode, FileAccess fileAccess)
+        {
+            WarnIfNoRoot();
+
+            CreateDirectoryIfApplicable(path, fileMode);
+
+            return File.Open(path, fileMode, fileAccess);
+        }
+
+        public Stream OpenFile(string path, FileMode fileMode, FileAccess fileAccess, FileShare fileShare)
+        {
+            WarnIfNoRoot();
+
+            CreateDirectoryIfApplicable(path, fileMode);
+
+            return File.Open(path, fileMode, fileAccess, fileShare);
+        }
+
+        private void CreateDirectoryIfApplicable(string path, FileMode fileMode)
+        {
+            if (fileMode == FileMode.Create ||
+                fileMode == FileMode.CreateNew ||
+                fileMode == FileMode.OpenOrCreate)
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+            }
+        }
+
+        private void WarnIfNoRoot()
+        {
+            if (appDataFolderSet)
+                return;
+
+            if (noRootWarning)
+                return;
+
+            Log.Warn($"No root folder set. User data is stored in {rootFolder}");
+            noRootWarning = true;
+        }
+#endif
+
+        #region --- UWP ---
+#if WINDOWS_UWP || ANDROID
         private IsolatedStorageFile iso;
 
         public UserStorage()
         {
-#if WINDOWS_UWP
             iso = IsolatedStorageFile.GetUserStoreForApplication();
-#else
-            iso = IsolatedStorageFile.GetUserStoreForDomain();
-#endif
+            
+            // If the above line doesn't work, this one might suffice.
+            // iso = IsolatedStorageFile.GetUserStoreForDomain();
+        }
+
+        public void SetApplicationDataFolder(string folderName)
+        {
+            // no-op, since we're using isolated storage.
         }
 
         public bool FileExists(string path)
@@ -69,5 +164,8 @@ namespace AgateLib.Storage
                 iso.CreateDirectory(Path.GetDirectoryName(path));
             }
         }
+#endif
+
+        #endregion
     }
 }
