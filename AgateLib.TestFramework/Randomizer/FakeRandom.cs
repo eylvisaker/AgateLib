3d0cefc56176b22a50d91bbb2938f74c55ad43e0
Erik Ylvisaker <eylvisaker@gmail.com>
2019-06-17 17:08:52 -0700

    Merge branch 'combat' into devel

---
+++ AgateLib.TestFramework/Randomizer/FakeRandom.cs
@@ -0,0 +1,15 @@
+﻿using AgateLib.Randomizer;
+
+namespace AgateLib.Randomizer
+{
+    public class FakeRandom : IRandom
+    {
+        public long Seed => 1;
+
+        public int NextIntegerMaxValue => short.MaxValue;
+
+        public double NextDouble() => 0.5;
+        public int NextInteger() => NextIntegerMaxValue / 2;
+        public float NextSingle() => 0.5f;
+    }
+}
