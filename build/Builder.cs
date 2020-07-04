a425456294f30bf2a3cda0984fd03a310906463e
Erik Ylvisaker <eylvisaker@gmail.com>
2020-07-04 11:31:02 -0700

    Jenkinsfile added.

--- build/Builder.cs
+++ build/Builder.cs
@@ -25,8 +25,31 @@ class Builder : NukeBuild
 
     public static int Main () => Execute<Builder>(x => x.Build);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly AgateLibConfiguration Configuration = AgateLibConfiguration.Debug;
+    #region --- Versioning ---
+
+    [Parameter("Overrides the branch name from git.")]
+    readonly string BranchName;
+
+    [Parameter("Sets the third number in the version 1.2.X.4")]
+    readonly string BuildNumber = "0";
+
+    private string Version
+    {
+        get
+        {
+            string version = System.IO.File.ReadAllText("version.info");
+            version += "." + BuildNumber.ToString();
+
+            if (!string.IsNullOrWhiteSpace(BranchName) && BranchName != "master")
+            {
+                version += "-" + BranchName.Replace("/", "-");
+            }
+
+            return version;
+        }
+    }
+
+    #endregion
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
@@ -86,6 +109,53 @@ class Builder : NukeBuild
                 .EnableNoRestore());
         });
         
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetVersion(Version)
+                .SetConfiguration("Release_DesktopGL")
+                .SetProject("src/AgateLib/AgateLib.csproj"));
+
+            DotNetPack(s => s
+                .SetVersion(Version)
+                .SetConfiguration("Release_WindowsDX")
+                .SetProject("src/AgateLib/AgateLib.csproj"));
+        });
+
+    #region --- Publish ---
+
+    Target Publish => _ => _
+        .DependsOn(Pack)
+        .Executes(() => 
+        {
+            GlobFiles(ArtifactsDirectory, "**/*.nupkg")
+               .NotEmpty()
+               .ForEach(x =>
+               {
+                   if (BranchName != "main" && BranchName != "devel") 
+                   {
+                       Console.WriteLine($"File {x} is not designated for pushing as release or prerelease Nuget package.");
+                       return;
+                   }
+
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(NugetApiUrl)
+                       .SetApiKey(NugetApiKey)
+                   );
+               });
+        });
+        
+    [Parameter("The NuGet API key for publishing")]
+    readonly string NugetApiKey = "";
+
+    [Parameter] 
+    readonly string NugetApiUrl = "https://api.nuget.org/v3/index.json"; //default
+
+    #endregion
+    
     // Target Build_Android => _ => _
     //     .DependsOn(Test)
     //     .Executes(() =>
@@ -95,14 +165,4 @@ class Builder : NukeBuild
     //             .SetConfiguration("Release_Android")
     //             .EnableNoRestore());
     //     });
-}
-
-
-public enum AgateLibConfiguration
-{
-    Debug,
-    Release_WindowsDX,
-    Release_DesktopGL,
-    Release_WindowsUniversal,
-    Release_Android
 }
